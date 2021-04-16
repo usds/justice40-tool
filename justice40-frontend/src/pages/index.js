efefb463fd5c356011e1e58fcccefb279b0c650f
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-04-16 16:28:08 -0400

    Adding leaflet example loading direct from geojson file

--- justice40-frontend/src/pages/index.js
+++ justice40-frontend/src/pages/index.js
@@ -3,8 +3,8 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Tagline from '../components/tagline';
 import { Helmet } from "react-helmet";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, FeatureGroup } from 'react-leaflet'
+import geojson from "../data/maryland.json";
 
 // markup
 const IndexPage = () => {
@@ -17,14 +17,25 @@ const IndexPage = () => {
       <SEO title="Justice 40 Home" />
       
       <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
+          center={[39.0458, -76.6413]}
+          zoom={9}
           style={{ height: "400px" }}
         >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
           />
+
+          <FeatureGroup 
+            pathOptions={{ weight:0.1, 
+                           color: 'black', 
+                          fillColor: 'grey',  
+                          "fill-opacity": 0.5 }}
+          >
+            <GeoJSON data={geojson}></GeoJSON>
+          </FeatureGroup>
+         
+        
           <Marker position={[51.505, -0.09]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
