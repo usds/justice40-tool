ead6bc6d1fb188ff9f054a1f9adeaa3bfe91205e
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-04-15 20:52:48 -0400

    Dockerizing pg_tileserv. Setup, healthcheck, and init work but load data does not load.

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# Tile Server
+
+A simple tile server using [pg_tileserv](https://github.com/CrunchyData/pg_tileserv).
+
+Based on pg_tileserv [docker example](https://github.com/CrunchyData/pg_tileserv/tree/master/examples/docker).
---
+++ server/load-data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Load Maryland geojson
+echo "Loading data..."
+docker-compose exec tileserv_db sh -c 'ogr2ogr -progress PG:"dbname=tileserv user=tileserv" /work/maryland.geojson'
+echo "Data load complete"
