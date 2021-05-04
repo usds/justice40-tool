9275966c0a9e1be7789ad28e475cbd5d4eaff165
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-05-04 14:56:49 -0400

    Simplifying, adding initialization script to docker-compose

--- server/load-data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Load Maryland geojson
-echo "Loading data..."
-docker-compose exec tileserv_db sh -c 'ogr2ogr -progress -f PostgreSQL PG:"dbname=tileserv user=tileserv" /work/maryland.geojson -nln maryland'
-echo "Data load complete"
