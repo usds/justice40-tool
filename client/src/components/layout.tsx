8f838da8a70ed2e4f60d9126df644920ad2877ec
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-05-19 13:23:22 -0400

    attempting to work arround 'SSR Error' -- still seeing : Warning: React.createElement: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,19 +1,21 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import * as styles from './layout.module.css';
 
 import { GovBanner } from '@trussworks/react-uswds';
 
 interface ILayoutProps {
-    children: ReactNode;
+    children: React.ReactNode;
 }
 
-const Layout = ({ children }: ILayoutProps) => {
+const Layout: React.FC<ILayoutProps> = ({ children }) => {
     return (
         <>
-        <GovBanner />
-        <div className={styles.siteContent}>{children}</div>
+            <GovBanner />
+            <div className={styles.siteContent}>
+                {children}
+            </div>
         </>
     );
 };
 
-  export default Layout;
+export default Layout;
