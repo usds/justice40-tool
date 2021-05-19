c18990c7997828dff1a10df771aee405fb819b62
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-05-19 17:59:09 -0400

    #16 pt 2 - adding typescript

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <>
+      <main>
+        <title>Justice40</title>
+        <h1>Justice40</h1>
+      </main>
+    </>
+  )
+}
+
+export default IndexPage
