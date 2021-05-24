7ce56d0aa2cad8b2f320c3101418f342688fc9ba
Jorge Escobar <jorge.e.escobar@omb.eop.gov>
2021-05-24 14:20:20 -0400

    added descriptions; email field; static Google Form file

--- data-roadmap/data_roadmap/data_set_descriptions/google_form_gen.js
+++ data-roadmap/data_roadmap/data_set_descriptions/google_form_gen.js
@@ -1,15 +1,31 @@
 function createForm() {
+  var form = FormApp.openByUrl(
+    "https://docs.google.com/forms/d/1Kb9Hl82I9kPJhqS_3bQDbWWvzCxPDTZBaI7_Eo6339E/edit"
+  );
+  var items = form.getItems();
+  while (items.length > 0) {
+    form.deleteItem(items.pop());
+  }
+
   var item = "Justice 40 Data Set Submission";
-  var form = FormApp.create(item).setTitle(item);
+  var form = form.setTitle(item);
+
+  var description =
+    "Thank you for participating in the Justice40 open data roadmap! Use this form to submit individual data sets to be considered for the data roadmap of the Justice40 Climate and Environmental Justice Screening Tool. Once the data set is submitted, you can view it in our submission tracking spreadsheet here (https://docs.google.com/spreadsheets/d/14Zwja62gbrZErhf70lo-I2ode85O-XZC1NKA7bEV6Bk/edit?resourcekey#gid=546636493).\n\nPlease try to fill out as many of the details below as possible for the data set you are submitting. If you do not know the value for a given field, you can leave it blank, and someone from our team or from the open source community will help research to figure it out and will update the spreadsheet accordingly. Instructions for suggesting updates to the spreadsheet are available in the second tab of the spreadsheet. Once a data set has been fully researched, the White House Environment Justice Advisory Council (WHEJAC) will collaborate to determine whether and how to prioritize the data set for inclusion in the tool.\n\nIf you have any questions, please email us at justice40open@usds.gov.";
+  form.setDescription(description);
 
   var item = "Data Set Name";
-  form.addTextItem().setTitle(item).setRequired(true);
+  var description = "A short name for the data set.";
+  form.addTextItem().setTitle(item).setHelpText(description).setRequired(true);
 
   var item = "Data Set Source Full URL";
-  form.addTextItem().setTitle(item).setRequired(true);
+  var description = "The URL / website where this data set can be found.";
+  form.addTextItem().setTitle(item).setHelpText(description).setRequired(true);
 
   var item = "Relevance to Environmental Justice";
-  form.addTextItem().setTitle(item).setRequired(false);
+  var description =
+    "Reason(s) why this data set is important for the environmental justice community (e.g. helps identify an EJ community)";
+  form.addTextItem().setTitle(item).setHelpText(description).setRequired(false);
 
   var item = "Data Formats";
   choices = [
@@ -22,13 +38,14 @@ function createForm() {
     "GDB",
     "MBTILES",
     "LAS",
+    "Not Sure",
     "Other",
   ];
   form
     .addMultipleChoiceItem()
     .setTitle(item)
     .setChoiceValues(choices)
-    .setRequired(true);
+    .setRequired(false);
 
   var item = "Spatial Resolution";
   var choices = [
@@ -40,11 +57,13 @@ function createForm() {
     "Exact address or lat/long",
     "Other",
   ];
+  var description = "How specific does this data set get?";
   form
     .addMultipleChoiceItem()
     .setTitle(item)
     .setChoiceValues(choices)
-    .setRequired(true);
+    .setHelpText(description)
+    .setRequired(false);
 
   var item = "Public Status";
   var choices = [
@@ -53,25 +72,37 @@ function createForm() {
     "Public for certain audiences",
     "Other",
   ];
+  var description = "Has this data set gone through a public release process?";
   form
     .addMultipleChoiceItem()
     .setTitle(item)
     .setChoiceValues(choices)
-    .setRequired(true);
+    .setHelpText(description)
+    .setRequired(false);
 
   var item = "Sponsor";
-  form.addTextItem().setTitle(item).setRequired(true);
+  var description =
+    "Who is providing and maintaining this data? (e.g. a federal agency, a specific organization)";
+  form.addTextItem().setTitle(item).setHelpText(description).setRequired(false);
 
   var item = "Subjective Rating of Data Quality";
+  var description = "Has this data set been vetted?";
   var choices = ["Low Quality", "Medium Quality", "High Quality"];
   form
     .addMultipleChoiceItem()
     .setTitle(item)
+    .setHelpText(description)
     .setChoiceValues(choices)
     .setRequired(false);
 
   var item = "Estimated Margin of Error";
-  var textItem = form.addTextItem().setTitle(item).setRequired(false);
+  var description =
+    "Estimated margin of error on measurement, if known. (1-100)";
+  var textItem = form
+    .addTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
   var textValidation = FormApp.createTextValidation()
     .setHelpText("Input was not a number between 1 and 100.")
     .requireNumberBetween(1, 100)
@@ -79,10 +110,21 @@ function createForm() {
   textItem.setValidation(textValidation);
 
   var item = "Known Data Quality Issues";
-  form.addParagraphTextItem().setTitle(item).setRequired(false);
+  var description = "Are there any known issues with this data set?";
+  form
+    .addParagraphTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
 
-  var item = "Geographic Coverage Percent";
-  var textItem = form.addTextItem().setTitle(item).setRequired(false);
+  var item = "Geographic Coverage Percent (1-100)";
+  var description =
+    "What percentage of the United States and US territories does this data set cover? This can be an estimate.";
+  var textItem = form
+    .addTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
   var textValidation = FormApp.createTextValidation()
     .setHelpText("Input was not a number between 1 and 100.")
     .requireNumberBetween(1, 100)
@@ -90,12 +132,19 @@ function createForm() {
   textItem.setValidation(textValidation);
 
   var item = "Geographic Coverage Description";
-  form.addParagraphTextItem().setTitle(item).setRequired(false);
+  var description = "Tell us more about the geographic coverage.";
+  form
+    .addParagraphTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
 
   var item = "Last Updated Date";
-  form.addDateItem().setTitle(item).setRequired(true);
+  var description = "When was the data last updated or refreshed?";
+  form.addDateItem().setTitle(item).setHelpText(description).setRequired(false);
 
   var item = "Frequency of Updates";
+  var description = "How often is this data updated?";
   var choices = [
     "Less than annually",
     "Approximately annually",
@@ -106,26 +155,48 @@ function createForm() {
   form
     .addMultipleChoiceItem()
     .setTitle(item)
-    .setChoiceValues(choices)
-    .setRequired(true);
-
-  var item = "Documentation";
-  form.addParagraphTextItem().setTitle(item).setRequired(false);
-
-  var item = "Data can go in Cloud";
-  var choices = ["Yes", "No"];
-  form
-    .addMultipleChoiceItem()
-    .setTitle(item)
+    .setHelpText(description)
     .setChoiceValues(choices)
     .setRequired(false);
 
-  var item = "Discussion";
-  form.addParagraphTextItem().setTitle(item).setRequired(false);
+  var item = "Documentation";
+  var description = "Please include any links to documentation.";
+  form
+    .addParagraphTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
 
-  var random_id = Utilities.getUuid();
-  var ss = SpreadsheetApp.create(
-    "Justice 40 Data Set Subnmissions - version: " + random_id
+  var item = "Can data go in the cloud?";
+  var choices = ["Yes", "No"];
+  var description = "Some datasets can not legally go in the cloud";
+  form
+    .addMultipleChoiceItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setChoiceValues(choices)
+    .setRequired(false);
+
+  var item = "Additional Information";
+  var description =
+    "Please include additional information around peer review, data availability (is it available from multiple sources?), any legal considerations, accreditation, etc.";
+  form
+    .addParagraphTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
+
+  var item = "Email address of submitter";
+  var description =
+    "If you would like for us to contact you for any followups. Note: your email will be visible by others.";
+  form
+    .addParagraphTextItem()
+    .setTitle(item)
+    .setHelpText(description)
+    .setRequired(false);
+
+  var ss = SpreadsheetApp.openByUrl(
+    "https://docs.google.com/spreadsheets/d/14Zwja62gbrZErhf70lo-I2ode85O-XZC1NKA7bEV6Bk/edit"
   );
   form.setDestination(FormApp.DestinationType.SPREADSHEET, ss.getId());
 }
