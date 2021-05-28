7ab7cd2a7a46dbaac2a6b6a89ab9a65bb053d2a1
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-05-28 19:11:59 -0400

    adding failure logging

--- client/cypress/e2e/accessibility.test.js
+++ client/cypress/e2e/accessibility.test.js
@@ -1,10 +1,34 @@
 // / <reference types="Cypress" />
 
+// Define at the top of the spec file or just import it
+
+// eslint-disable-next-line require-jsdoc
+function terminalLog(violations) {
+  cy.task(
+      'log',
+      `${violations.length} accessibility violation${
+        violations.length === 1 ? '' : 's'
+      } ${violations.length === 1 ? 'was' : 'were'} detected`,
+  );
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+      ({id, impact, description, nodes}) => ({
+        id,
+        impact,
+        description,
+        nodes: nodes.length,
+      }),
+  );
+
+  cy.task('table', violationData);
+}
+
+
 describe('Accessibility tests', () => {
   beforeEach(() => {
     cy.visit('/').get('main').injectAxe();
   });
   it('Has no detectable accessibility violations on load', () => {
-    cy.checkA11y();
+    cy.checkA11y(null, null, terminalLog);
   });
 });
--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -19,4 +19,16 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('task', {
+    log(message) {
+      console.log(message);
+
+      return null;
+    },
+    table(message) {
+      console.table(message);
+
+      return null;
+    },
+  });
 };
