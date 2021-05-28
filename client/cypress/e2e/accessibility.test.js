aee2ff39b56a55d35bce1b5ad5eafb84f19f4af6
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-05-28 18:58:39 -0400

    Adding accessibility testing support and basic a11y tests

---
+++ client/cypress/e2e/accessibility.test.js
@@ -0,0 +1,10 @@
+// / <reference types="Cypress" />
+
+describe('Accessibility tests', () => {
+  beforeEach(() => {
+    cy.visit('/').get('main').injectAxe();
+  });
+  it('Has no detectable accessibility violations on load', () => {
+    cy.checkA11y();
+  });
+});
--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -13,8 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
 import './commands';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+import 'cypress-axe';
+import '@testing-library/cypress/add-commands';
