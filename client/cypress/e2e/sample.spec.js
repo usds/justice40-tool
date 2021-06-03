426f596c7aa92986a192947be1d7e1982aa27bd0
Nat Hillard <72811320+NatHillardUSDS@users.noreply.github.com>
2021-06-02 20:53:22 -0400

    Adding Cypress for e2e testing (#85)
    * using the higher-level react-testing-library, and regenerating snapshot - renders real DOM elements
    * Basic e2e testing with Cypress, following the guide here: https://www.gatsbyjs.com/docs/how-to/testing/end-to-end-testing/ ; needed to install cypress-local to avoid jest-cypress collision
    * Adding accessibility testing support and basic a11y tests
    * adding failure logging
    * Adding nightly test run
    * Fix misc stuff from lighthouse (#81)
    * Removing local-cypress, relying instead on a combination
    of type reference and eslint-plugin-cypress;
    adding cypress to jest ignore paths to avoid conflict -
    `npm test` is now jest-only, use `npm run test:e2e` to run cypress tests
    * updating comment to clarify timezone

---
+++ client/cypress/e2e/sample.spec.js
@@ -0,0 +1,11 @@
+// / <reference types="Cypress" />
+
+describe('Translation Test', () => {
+  it('Checks that locales have correct content', () => {
+    cy.visit('http://localhost:8000');
+    cy.url().should('include', '/en/');
+    cy.get('header').contains('Justice40');
+    cy.visit('http://localhost:8000/es');
+    cy.get('header').contains('Justicia40');
+  });
+});
