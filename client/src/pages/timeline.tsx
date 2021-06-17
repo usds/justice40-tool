e7ccd35aa6872722c8e4dcfbdf33f62049a6d20b
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2021-06-17 07:31:18 -0700

    Minimal refactor to make maps wider (#149)
    Not particularly pretty, but it works.

--- client/src/pages/timeline.tsx
+++ client/src/pages/timeline.tsx
@@ -10,51 +10,53 @@ interface TimelinePageProps {
 
 const TimelinePage = ({location}: TimelinePageProps) => {
   return (<Layout location={location}>
-    <h1>Timeline</h1>
-    <h2>Throughout the Process</h2>
-    <div className="grid-col">
-      <div className="grid-row grid-gap-lg">
-        <div className="grid-col-1">
-          <img
-            className={'flex-align-self-center width-4'}
-            src={renewIcon} alt={'renew icon'}/>
-        </div>
-        <div className={'usa-prose text-asset-container ' +
-        'grid-col-fill'}>
-          <p>Continuously engage with stakeholders and community
-            members to get feedback on the screening tool, scoring,
-            and overall process.</p>
-          <p>Continuously source data that meets data principles as
-            defined through community input.</p>
+    <section className={'usa-prose'}>
+      <h1>Timeline</h1>
+      <h2>Throughout the Process</h2>
+      <div className="grid-col">
+        <div className="grid-row grid-gap-lg">
+          <div className="grid-col-1">
+            <img
+              className={'flex-align-self-center width-4'}
+              src={renewIcon} alt={'renew icon'}/>
+          </div>
+          <div className={'usa-prose text-asset-container ' +
+          'grid-col-fill'}>
+            <p>Continuously engage with stakeholders and community
+              members to get feedback on the screening tool, scoring,
+              and overall process.</p>
+            <p>Continuously source data that meets data principles as
+              defined through community input.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <h2>Milestones</h2>
-    <ol className={'usa-process-list'}>
-      <li
-        className={'j40-usa-process-list__item--complete ' +
-          'usa-process-list__item padding-bottom-4 '}
-        aria-label={'milestone 1 done'}>
-        <h4 className={'usa-process-list__heading'}>Milestone 1</h4>
-        <p className={'margin-top-05'}>
-          Publish data principles on this site by June 2021.</p>
-      </li>
-      <li
-        className={'usa-process-list__item padding-bottom-4'}
-        aria-label={'milestone 2 next'}>
-        <h4 className={'usa-process-list__heading'}>Milestone 2</h4>
-        <p>Make the first version of a screening tool
-          available by July 2021.</p>
-      </li>
-      <li
-        className={'usa-process-list__item padding-bottom-4'}
-        aria-label={'milestone 3'}>
-        <h4 className={'usa-process-list__heading'}>Milestone 3</h4>
-        <p>Create a public scorecard to ensure accountability of
-            investments by February 2022.</p>
-      </li>
-    </ol>
+      <h2>Milestones</h2>
+      <ol className={'usa-process-list'}>
+        <li
+          className={'j40-usa-process-list__item--complete ' +
+            'usa-process-list__item padding-bottom-4 '}
+          aria-label={'milestone 1 done'}>
+          <h4 className={'usa-process-list__heading'}>Milestone 1</h4>
+          <p className={'margin-top-05'}>
+            Publish data principles on this site by June 2021.</p>
+        </li>
+        <li
+          className={'usa-process-list__item padding-bottom-4'}
+          aria-label={'milestone 2 next'}>
+          <h4 className={'usa-process-list__heading'}>Milestone 2</h4>
+          <p>Make the first version of a screening tool
+            available by July 2021.</p>
+        </li>
+        <li
+          className={'usa-process-list__item padding-bottom-4'}
+          aria-label={'milestone 3'}>
+          <h4 className={'usa-process-list__heading'}>Milestone 3</h4>
+          <p>Create a public scorecard to ensure accountability of
+              investments by February 2022.</p>
+        </li>
+      </ol>
+    </section>
   </Layout>
   );
 };
