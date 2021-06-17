f995c36cc9629ff7625f01d0a488c204586b5806
Jorge Escobar <jorge.e.escobar@omb.eop.gov>
2021-06-17 13:05:16 -0400

    failing CSV merge -- investigating

--- score/scripts/generate_mbtiles.py
+++ score/scripts/generate_mbtiles.py
@@ -23,8 +23,30 @@ for file in os.listdir(geojson_path):
         geojson_list += f"/home/data/census/geojson/{file} "
 
 if geojson_list == "":
-    print("No GeoJson files found. Please run download_cbg.py first")
+    print("No GeoJson files found. Please run scripts/download_cbg.py first")
 
+# Merge scores into json
+# TODO: for this first pass, just merging ACS EJScren indicators
+#       Per https://github.com/usds/justice40-tool/issues/102
+# -sql "SELECT * FROM tabblock2010_01_pophu LEFT JOIN '/home/custom.csv'.custom ON tabblock2010_01_pophu.BLOCKID10 = custom.BLOCKID10" /home/tabblock2010_01_pophu.json /vsizip//home/tabblock2010_01_pophu.zip
+
+if os.name == "nt":
+    pwd = "%cd%"
+else:
+    pwd = "${PWD}"
+cmd = (
+    'docker run --rm -v "'
+    + pwd
+    + '"/:/home '
+    + "osgeo/gdal:alpine-small-latest ogr2ogr -f GeoJSON "
+    + "-sql \"SELECT * FROM tl_2010_01_bg10 LEFT JOIN '/home/data/dataset/ejscreen_2020/01.csv'.custom ON tl_2010_01_bg10.GEOID10 = custom.ID\" "
+    + "/home/data/score/geojson/01.json /home/data/census/shp/01/tl_2010_01_bg10.dbf"
+)
+breakpoint()
+print(cmd)
+os.system(cmd)
+
+# generate mbtiles file
 # PWD is different for Windows
 if os.name == "nt":
     pwd = "%cd%"
