b90dbfe8767741a04b295c1a1f3d9d30fe5fb1d5
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-07-02 19:48:53 -0700

    adds initial download CBG box

--- client/src/pages/cejst.module.scss.d.ts
+++ client/src/pages/cejst.module.scss.d.ts
@@ -1,6 +1,15 @@
 declare namespace CejstModuleScssNamespace {
   export interface ICejstModuleScss {
     disclaimer: string;
+    textBox: string;
+    downloadBoxContainer: string;
+    downloadBox: string;
+    downloadBoxTextBox: string;
+    downloadBoxTitle: string;
+    downloadBoxText: string;
+    downloadBoxButtonContainer: string;
+    downloadBoxButton: string;
+    downloadPacketText: string;
   }
 }
 
--- client/src/pages/cejst.tsx
+++ client/src/pages/cejst.tsx
@@ -4,7 +4,9 @@ import MapWrapper from '../components/mapWrapper';
 import HowYouCanHelp from '../components/HowYouCanHelp';
 import MapLegend from '../components/mapLegend';
 import * as styles from './cejst.module.scss';
-
+import {Button} from '@trussworks/react-uswds';
+// @ts-ignore
+import downloadIcon from '/node_modules/uswds/dist/img/usa-icons/file_download.svg';
 
 interface IMapPageProps {
   location: Location;
@@ -16,24 +18,60 @@ const CEJSTPage = ({location}: IMapPageProps) => {
   return (
     <Layout location={location}>
       <main id="main-content" role="main">
-        <h2>Just Progress communities</h2>
-        <p className={styles.disclaimer}>
-          Just Progress helps identify and prioritize communities across the
-          United States and U.S. territories that have been historically
-          overburdened and underserved. These communities will receive 40% of
-          the benefits from investments in key areas outlined by the
-          <a
-            href={'https://www.whitehouse.gov/briefing-room/' +
-            'presidential-actions/2021/01/27/' +
-            'executive-order-on-tackling-the-climate-' +
-            'crisis-at-home-and-abroad/'}
-            target={'_blank'}
-            rel={'noreferrer'}>
-            Executive Order on Tackling the Climate Crisis at Home and
-            Abroad</a>.
-        </p>
+
+        <section>
+          <h2>Just Progress communities</h2>
+          <div className={styles.disclaimer}>
+            <div className={styles.textBox}>
+              <p>
+                Just Progress helps identify and prioritize communities across the United States and U.S. territories
+                that have been historically overburdened and underserved. These communities will receive 40% of
+                the benefits from investments in key areas outlined by the
+                &nbsp;
+                <a
+                  href={'https://www.whitehouse.gov/briefing-room/' +
+                    'presidential-actions/2021/01/27/' +
+                    'executive-order-on-tackling-the-climate-' +
+                    'crisis-at-home-and-abroad/'}
+                  target={'_blank'}
+                  rel={'noreferrer'}>
+                  Executive Order on Tackling the Climate Crisis at Home and Abroad
+                </a>.
+              </p>
+              <p>
+                Download the Just Progress packet or explore the map below to see the list of prioritized communites. To
+                learn more about how these communities were prioritized check out the
+                &nbsp;
+                <a
+                  href={'./methodology'}>
+                  Methodology
+                </a>
+                &nbsp;
+                page.
+              </p>
+            </div>
+            <div className={styles.downloadBoxContainer}>
+              <div className={styles.downloadBox}>
+                <div className={styles.downloadBoxTextBox}>
+                  <div className={styles.downloadBoxTitle}>Just Progress Packet</div>
+                  <div className={styles.downloadBoxText}>This downloadable packet includes the list of Just Progress
+                  prioritized communities (30,021
+                    census block groups) and 18 datasets.
+                  </div>
+                  <div className={styles.downloadBoxButtonContainer}>
+                    <Button className={styles.downloadBoxButton} type="button">
+                      <div><img src={downloadIcon} /> </div>
+                      <div className={styles.downloadPacketText}>Download packet</div>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <h2>Explore the Tool</h2>
-        <MapWrapper/>
+        <MapWrapper />
         <MapLegend />
         <HowYouCanHelp />
       </main>
