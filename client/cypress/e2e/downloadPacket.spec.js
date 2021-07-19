e5908cf7d735dd395fbe68f8d59d9d3e6916a32d
Vim <86254807+vim-usds@users.noreply.github.com>
2021-07-19 08:05:32 -0700

    updates filename and comments (#378)

--- client/cypress/e2e/downloadPacket.spec.js
+++ client/cypress/e2e/downloadPacket.spec.js
@@ -2,9 +2,10 @@
 
 describe('Census Block Group download', () => {
   it('validate file download', () => {
+    const filename = `usa.zip`;
     cy.visit('localhost:8000/en/cejst');
     cy.get('#download-link').invoke('attr', 'target', '_blank');
-    cy.intercept('https://justice40-data.s3.amazonaws.com/Score/usa.zip',
+    cy.intercept(`https://justice40-data.s3.amazonaws.com/Score/${filename}`,
         {
           body: 'success',
           headers: {
@@ -15,6 +16,6 @@ describe('Census Block Group download', () => {
     ).as('downloadRequest');
     cy.get('button[class*="downloadPacket"]').click();
     cy.wait('@downloadRequest');
-    cy.readFile(`cypress/downloads/usa.csv`).should('exist');
+    cy.readFile(`cypress/downloads/${filename}`).should('exist');
   });
 });
