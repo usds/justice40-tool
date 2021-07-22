ee734e0164602cda1839b7ee71816a4ce4deea15
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-07-22 09:49:46 -0400

    fixes failing latlng url test

--- client/cypress/e2e/latlngurl.spec.js
+++ client/cypress/e2e/latlngurl.spec.js
@@ -1,11 +1,15 @@
 // / <reference types="Cypress" />
 
 describe('LatLng Test', () => {
-  it('Checks that as the map zooms the lat/lng coordinates in the URL update', () => {
-    cy.visit('http://localhost:8000/en/cejst?flags=mb');
+  it('URL starts at zoom level 3', () => {
+    cy.visit('http://localhost:8000/en/cejst');
     cy.url().should('include', '#3');
-    cy.get('.maplibregl-ctrl-zoom-in > .maplibregl-ctrl-icon').click();
+  });
+  it('URL changes to level 4 when you hit the zoom button', () => {
+    cy.get('.mapboxgl-ctrl-zoom-in > .mapboxgl-ctrl-icon').click();
     cy.url().should('include', '#4');
+  });
+  it('URL includes correct lat/lng coordinates', () => {
     cy.getMap().then((map) => {
       cy.panTo(map, [-77.9, 35.04]);
       cy.url().should('include', '#4/35.04/-77.9');
