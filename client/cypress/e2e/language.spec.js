1f69a66226827eca3118e6adcaf2369e576fb68b
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2021-07-22 09:34:25 -0400

    updating translation tests to pass

--- client/cypress/e2e/language.spec.js
+++ client/cypress/e2e/language.spec.js
@@ -1,11 +1,14 @@
 // / <reference types="Cypress" />
 
 describe('Translation Test', () => {
-  it('Checks that locales have correct content', () => {
+  it('Sets default language to /en and redirects', () => {
     cy.visit('http://localhost:8000');
     cy.url().should('include', '/en/');
-    cy.get('header').contains('Justice40');
+    cy.get('h1').contains('About Justice40');
+  });
+
+  it('Sets page content to spanish when visiting Spanish URL', () => {
     cy.visit('http://localhost:8000/es');
-    cy.get('header').contains('Justicia40');
+    cy.get('h1').contains('Acerca de Justice40');
   });
 });
