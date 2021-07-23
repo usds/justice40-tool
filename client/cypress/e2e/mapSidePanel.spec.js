1076551dda5545598860cf80ab1e2533f774748e
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-07-23 15:25:56 -0700

    adds cypress tests on desktop and mobile

--- client/cypress/e2e/mapSidePanel.spec.js
+++ client/cypress/e2e/mapSidePanel.spec.js
@@ -1,39 +1,108 @@
 // / <reference types="Cypress" />
 
-// describe('Map side panel', () => {
-//   it('should show the map introduction text', () => {
-//     cy.visit('http://localhost:8000/en/cejst');
+/*
+A risk with this test is that if the feature/area that is currently being selected become non-prioritized, then this
+test will fail. However it would be a major win for that area!
+*/
 
-//     // validate that side panel intro text is show
-//     cy.get('div[class*="mapIntroHeader"]').scrollIntoView();
-//     cy.get('div[class*="mapIntroHeader"]').contains('Zoom').should('be.visible');
+const devices = [
+  [1024, 720],
+  ['iphone-6', 'portrait'],
+  ['samsung-s10', 'portrait'],
+];
 
-//     // Simulate a click on the map
-//   });
-// });
+describe('tests that the map side panel renders MapIntroduction component', () => {
+  devices.forEach((device) => {
+    it(`should render MapIntroduction component on ${device[0]} x ${device[1]}`, () => {
+      cy.visit('http://localhost:8000/en/cejst');
+      cy.viewport(device[0], device[1]);
+      cy.get('div[class*="mapIntroContainer"]').should('be.visible');
+    });
+  });
+});
 
-// describe('Tests for the Explore the Map page', () => {
-//   beforeEach(() => {
-//     cy.viewport('macbook-13');
-//     cy.visit('http://localhost:8000/en/cejst');
-//   });
-
-//   // The below values all assume a 13-inch MB as set in viewport above.
-//   // Values will be different for different screens
-//   const tests = {
-//     'Lower 48': '3.25/38.07/-95.87',
-//     'Alaska': '3/63.28/-162.39',
-//     'Hawaii': '5.89/20.574/-161.438',
-//     'Puerto Rico': '8.19/18.2/-66.583',
-//   };
-
-//   for (const [territory, zxy] of Object.entries(tests)) {
-//     it(`Can focus on ${territory} `, () => {
+// Todo VS: Figure out why this forEach block doesn't work:
+// describe('tests that the map side panel renders AreaDetail component', () => {
+//   devices.forEach((device) => {
+//     it(`should render AreaDetail component on ${device[0]} x ${device[1]}`, () => {
+//       if (!device[0].isIteger) cy.viewport(device[0], device[1]);
+//       cy.visit('http://localhost:8000/en/cejst');
 //       cy.getMap().then((map) => {
-//         cy.get(`[aria-label="Focus on ${territory}"]`).click();
-//         cy.waitForMapIdle(map);
-//         cy.url().should('include', zxy);
+//         const zoomLevel = device[0].isInteger ? 11 : 10;
+//         for (let zoom=3; zoom<=zoomLevel; zoom++) {
+//           cy.get('.mapboxgl-ctrl-zoom-in > .mapboxgl-ctrl-icon').click();
+//           cy.waitForMapIdle(map);
+//         }
+//         cy.get('.overlays').click('bottomRight');
+//         cy.get('div[class*="areaDetailContainer"]').should('be.visible');
+//         cy.get('div[class*="score"]').should('be.visible');
+//         cy.get('div[class*="topRowSubTitle"]').should('be.visible');
+//         cy.get('div[class*="indicatorBox"]').should('be.visible');
+//         cy.get('div[class*="indicatorBox"]')
+//         // currently the padding-top on desktop = 1.5rem => 24px
+//         // currently the padding-top on mobile = .8 rem => 8px
+//             .each((indicator) => cy.wrap(indicator)
+//                 .should('have.css', 'padding-top', `${device[0].isInteger ? '24px' : '8px'}`));
 //       });
 //     });
-//   };
+//   });
 // });
+
+it('should render AreaDetail component on desktop', () => {
+  cy.visit('http://localhost:8000/en/cejst');
+  cy.getMap().then((map) => {
+    const zoomLevel = 11;
+    for (let zoom=3; zoom<=zoomLevel; zoom++) {
+      cy.get('.mapboxgl-ctrl-zoom-in > .mapboxgl-ctrl-icon').click();
+      cy.waitForMapIdle(map);
+    }
+    cy.get('.overlays').click('bottomRight');
+    cy.get('div[class*="areaDetailContainer"]').should('be.visible');
+    cy.get('div[class*="score"]').should('be.visible');
+    cy.get('div[class*="topRowSubTitle"]').should('be.visible');
+    cy.get('div[class*="indicatorBox"]').should('be.visible');
+    cy.get('div[class*="indicatorBox"]')
+    // currently the padding-top = 1.5rem => 24px
+        .each((indicator) => cy.wrap(indicator).should('have.css', 'padding-top', '24px'));
+  });
+});
+
+it('should render AreaDetail component on iPhone 6 (iOS)', () => {
+  cy.viewport('iphone-6');
+  cy.visit('http://localhost:8000/en/cejst');
+  cy.getMap().then((map) => {
+    const zoomLevel = 10;
+    for (let zoom=3; zoom<=zoomLevel; zoom++) {
+      cy.get('.mapboxgl-ctrl-zoom-in > .mapboxgl-ctrl-icon').click();
+      cy.waitForMapIdle(map);
+    }
+    cy.get('.overlays').click('bottomRight');
+    cy.get('div[class*="areaDetailContainer"]').should('be.visible');
+    cy.get('div[class*="score"]').should('be.visible');
+    cy.get('div[class*="topRowSubTitle"]').should('be.visible');
+    cy.get('div[class*="indicatorBox"]').should('be.visible');
+    cy.get('div[class*="indicatorBox"]')
+    // currently the padding-top = .5rem => 8px
+        .each((indicator) => cy.wrap(indicator).should('have.css', 'padding-top', '8px'));
+  });
+});
+
+it('should render AreaDetail component on Samsung Galaxy S10 (android)', () => {
+  cy.viewport('samsung-s10');
+  cy.visit('http://localhost:8000/en/cejst');
+  cy.getMap().then((map) => {
+    const zoomLevel = 10;
+    for (let zoom=3; zoom<=zoomLevel; zoom++) {
+      cy.get('.mapboxgl-ctrl-zoom-in > .mapboxgl-ctrl-icon').click();
+      cy.waitForMapIdle(map);
+    }
+    cy.get('.overlays').click('bottomRight');
+    cy.get('div[class*="areaDetailContainer"]').should('be.visible');
+    cy.get('div[class*="score"]').should('be.visible');
+    cy.get('div[class*="topRowSubTitle"]').should('be.visible');
+    cy.get('div[class*="indicatorBox"]').should('be.visible');
+    cy.get('div[class*="indicatorBox"]')
+    // currently the padding-top = .5rem => 8px
+        .each((indicator) => cy.wrap(indicator).should('have.css', 'padding-top', '8px'));
+  });
+});
