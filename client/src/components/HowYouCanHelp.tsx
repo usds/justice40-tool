eb313ec2284b80e0bd0993fc336c9b99b4f42ff2
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-08-03 08:30:02 -0700

    troubleshooting li element on deployed URL
    removing local bullet styles

--- client/src/components/HowYouCanHelp.tsx
+++ client/src/components/HowYouCanHelp.tsx
@@ -7,28 +7,29 @@ const HowYouCanHelp = () => {
       <h2>How You Can Help Improve the Tool</h2>
       <ul>
         <li>
-          <div className={styles.listWrapper}>
-            <div className={styles.howYouCanHelpBullet}></div>
-            <div className={styles.howYouCanHelpText}>If you have helpful information, we’d love to{` `}
-              <a href={'mailto:vimusds@gmail.com'}>get an email from you</a>
+          {/* Removing list styling to test on deployed URL */}
+          {/* <div className={styles.listWrapper}>
+            <div className={styles.howYouCanHelpBullet}></div> */}
+          <div className={styles.howYouCanHelpText}>If you have helpful information, we’d love to{` `}
+            <a href={'mailto:vimusds@gmail.com'}>get an email from you</a>
               .</div>
-          </div>
+          {/* </div> */}
         </li>
         <li>
-          <div className={styles.listWrapper}>
-            <div className={styles.howYouCanHelpBullet}></div>
-            <div className={styles.howYouCanHelpText}>View our{` `}
-              <a href={'methodology'}>data and methodology</a>{` `}
+          {/* <div className={styles.listWrapper}>
+            <div className={styles.howYouCanHelpBullet}></div> */}
+          <div className={styles.howYouCanHelpText}>View our{` `}
+            <a href={'methodology'}>data and methodology</a>{` `}
               and send us feedback.</div>
-          </div>
+          {/* </div> */}
         </li>
         <li>
-          <div className={styles.listWrapper}>
-            <div className={styles.howYouCanHelpBullet}></div>
-            <div className={styles.howYouCanHelpText}>Find your community and{` `}
-              <a href={'contact'}>share your feedback</a>
+          {/* <div className={styles.listWrapper}>
+            <div className={styles.howYouCanHelpBullet}></div> */}
+          <div className={styles.howYouCanHelpText}>Find your community and{` `}
+            <a href={'contact'}>share your feedback</a>
               .</div>
-          </div>
+          {/* </div> */}
         </li>
 
       </ul>
