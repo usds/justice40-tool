8fafbee44861a0c47b87929b3fbaadc77ef99058
Jorge Escobar <jorge.e.escobar@omb.eop.gov>
2021-08-03 17:35:11 -0400

    PR feedback

--- data/data-pipeline/tile/generate.py
+++ data/data-pipeline/tile/generate.py
@@ -15,6 +15,11 @@ def generate_tiles(data_path: Path) -> None:
     low_tile_path = score_tiles_path / "low"
     score_geojson_dir = data_path / "score" / "geojson"
 
+    USA_HIGH_MIN_ZOOM = 5
+    USA_HIGH_MAX_ZOOM = 11
+    USA_LOW_MIN_ZOOM = 0
+    USA_LOW_MAX_ZOOM = 7
+
     # remove existing mbtiles file
     remove_all_from_dir(score_tiles_path)
 
@@ -25,30 +30,31 @@ def generate_tiles(data_path: Path) -> None:
     # generate high mbtiles file
     logger.info(f"Generating USA High mbtiles file")
     cmd = "tippecanoe "
-    cmd += "--minimum-zoom=5 --maximum-zoom=11 --layer=blocks "
-    cmd += "--output=" + str(high_tile_path) + "/usa_high.mbtiles "
+    cmd += f"--minimum-zoom={USA_HIGH_MIN_ZOOM} --maximum-zoom={USA_HIGH_MAX_ZOOM} --layer=blocks "
+    cmd += f"--output={high_tile_path}/usa_high.mbtiles "
     cmd += str(score_geojson_dir / "usa-high.json")
     call(cmd, shell=True)
 
     # generate high mvts
     logger.info(f"Generating USA High mvt folders and files")
     cmd = "tippecanoe "
-    cmd += "--minimum-zoom=5 --maximum-zoom=11 --no-tile-compression "
-    cmd += "--output-to-directory=" + str(high_tile_path) + " "
+    cmd += f"--minimum-zoom={USA_HIGH_MIN_ZOOM} --maximum-zoom={USA_HIGH_MAX_ZOOM} --no-tile-compression "
+    cmd += f"--output-to-directory={high_tile_path} "
     cmd += str(score_geojson_dir / "usa-high.json")
     call(cmd, shell=True)
 
     # generate low mbtiles file
     logger.info(f"Generating USA Low mbtiles file")
     cmd = "tippecanoe "
-    cmd += "--minimum-zoom=0 --maximum-zoom=7 --layer=blocks "
-    cmd += "--output=" + str(low_tile_path) + "/usa_low.mbtiles "
+    cmd += f"--minimum-zoom={USA_LOW_MIN_ZOOM} --maximum-zoom={USA_LOW_MAX_ZOOM} --layer=blocks "
+    cmd += f"--output={low_tile_path}/usa_low.mbtiles "
     cmd += str(score_geojson_dir / "usa-low.json")
     call(cmd, shell=True)
 
     # generate low mvts
     logger.info(f"Generating USA Low mvt folders and files")
-    cmd = "tippecanoe --minimum-zoom=0 --maximum-zoom=7 --no-tile-compression "
-    cmd += "--output-to-directory=" + str(low_tile_path) + " "
+    cmd = "tippecanoe "
+    cmd += f"--minimum-zoom={USA_LOW_MIN_ZOOM} --maximum-zoom={USA_LOW_MAX_ZOOM} --no-tile-compression "
+    cmd += f"--output-to-directory={low_tile_path} "
     cmd += str(score_geojson_dir / "usa-low.json")
     call(cmd, shell=True)
