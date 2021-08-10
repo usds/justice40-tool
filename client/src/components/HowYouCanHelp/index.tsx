a8bf860fd98cb7114352932609941e3e80859e77
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-08-10 09:28:02 -0700

    add localization for HowYouCanHelp

--- client/src/components/HowYouCanHelp/index.tsx
+++ client/src/components/HowYouCanHelp/index.tsx
@@ -13,32 +13,79 @@ const HowYouCanHelp = () => {
       defaultMessage: 'How You Can Help Improve the Tool',
       description: 'the header of the how you can help section',
     },
+    youCanHelpInfoText: {
+      id: 'youCanHelpInfoText.list.element.prefix',
+      defaultMessage: 'If you have helpful information, we’d love to',
+      description: 'you can help info text ',
+    },
+    youCanHelpInfoLinkText: {
+      id: 'youCanHelpInfoLink.link.text',
+      defaultMessage: 'get an email from you',
+      description: 'you can help info text ',
+    },
+    youCanHelpDataMethPrefixText: {
+      id: 'youCanHelpDataMethPrefixText.link.prefix.text',
+      defaultMessage: 'View our',
+      description: 'view our',
+    },
+    youCanHelpDataMethLinkText: {
+      id: 'youCanHelpDataMethLinkText.link.text',
+      defaultMessage: 'Data & methodology',
+      description: 'Data & methodology link',
+    },
+    youCanHelpDataMethSuffixText: {
+      id: 'youCanHelpDataMethSuffixText.link.suffix.text',
+      defaultMessage: 'and send us feedback',
+      description: 'send us feedbackv via email',
+    },
+    youCanHelpSharingPrefixText: {
+      id: 'youCanHelpSharingPrefixText.link.prefix.text',
+      defaultMessage: 'Find your community and',
+      description: 'find your community',
+    },
+    youCanHelpSharingLinkText: {
+      id: 'youCanHelpSharingLinkText.link.text',
+      defaultMessage: 'share your feedback',
+      description: 'sharing link to email',
+    },
   });
 
   return (
     <div className={styles.howYouCanHelpContainer}>
-      <h2 className={styles.howYouCanHelpHeader}>{intl.formatMessage(messages.youCanHelpHeader)}</h2>
+      <h2 className={styles.howYouCanHelpHeader}>
+        {intl.formatMessage(messages.youCanHelpHeader)}
+      </h2>
       <ul className={styles.howYouCanHelpListWrapper}>
         <li className={styles.howYouCanHelpList}>
-          <div className={styles.howYouCanHelpText}>If you have helpful information, we’d love to
+          <div className={styles.howYouCanHelpText}>
+            {intl.formatMessage(messages.youCanHelpInfoText)}
             {` `}
-            <a href={'mailto:screeningtool.feedback@usds.gov'}>get an email from you</a>
-            {` `}
-            .</div>
+            <a href={'mailto:screeningtool.feedback@usds.gov'}>
+              {intl.formatMessage(messages.youCanHelpInfoLinkText)}
+            </a>
+            {` `}.
+          </div>
         </li>
         <li className={styles.howYouCanHelpList}>
-          <div className={styles.howYouCanHelpText}>View our
+          <div className={styles.howYouCanHelpText}>
+            {intl.formatMessage(messages.youCanHelpDataMethPrefixText)}
             {` `}
-            <Link to={'/methodology'}>Data & methodology</Link>
+            <Link to={'/methodology'}>
+              {intl.formatMessage(messages.youCanHelpDataMethLinkText)}
+            </Link>
             {` `}
-            and send us feedback.</div>
+            {intl.formatMessage(messages.youCanHelpDataMethSuffixText)}
+          </div>
         </li>
         <li className={styles.howYouCanHelpList}>
-          <div className={styles.howYouCanHelpText}>Find your community and
+          <div className={styles.howYouCanHelpText}>
+            {intl.formatMessage(messages.youCanHelpSharingPrefixText)}
             {` `}
-            <a href={'mailto:screeningtool.feedback@usds.gov'}>share your feedback</a>
-            {` `}
-            .</div>
+            <a href={'mailto:screeningtool.feedback@usds.gov'}>
+              {intl.formatMessage(messages.youCanHelpSharingLinkText)}
+            </a>
+            {` `}.
+          </div>
         </li>
       </ul>
     </div>
