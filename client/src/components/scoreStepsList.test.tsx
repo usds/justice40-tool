6ea4d6091014e00538ea5698ccce71885dc624fb
TomNUSDS <74203452+TomNUSDS@users.noreply.github.com>
2021-08-13 09:04:06 -0700

    Fix putting elements under <p>
    Added Jest test to catch this mistake in the future. It does it by watching console.error().
    To add in fixing the bug the test's emulated `console.error()` are echoed to the jest console.error() so they are not lost.
    
    I tested this by putting a `<h1>` back into the `<p>` and it caught it.

--- client/src/components/scoreStepsList.test.tsx
+++ client/src/components/scoreStepsList.test.tsx
@@ -3,6 +3,12 @@ import {render} from '@testing-library/react';
 import ScoreStepsList from './scoreStepsList';
 import {LocalizedComponent} from '../test/testHelpers';
 
+beforeAll(() => {
+  jest.spyOn(global.console, 'error').mockImplementation((...params) => {
+    console.error(params);
+  });
+});
+
 describe('rendering of the component', () => {
   const {asFragment} = render(
       <LocalizedComponent>
@@ -12,5 +18,9 @@ describe('rendering of the component', () => {
 
   it('should match the snapshot of the MapIntroduction component', () => {
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('No console errors', () => {
+    expect(console.error).toBeCalledTimes(0);
   });
 });
