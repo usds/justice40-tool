536a35d6a0467ffb2665728238a3c50bdd47ec98
Nat Hillard <72811320+NatHillardUSDS@users.noreply.github.com>
2021-09-10 14:17:34 -0400

    Data Unit Tests (#509)
    * Fixes #341 -
    As a J40 developer, I want to write Unit Tests for the ETL files,
    so that tests are run on each commit
    
    * Location bug
    
    * Adding Load tests
    
    * Fixing XLSX filename
    
    * Adding downloadable zip test
    
    * updating pickle
    
    * Fixing pylint warnings
    
    * Updte readme to correct some typos and reorganize test content structure
    
    * Removing unused schemas file, adding details to readme around pickles, per PR feedback
    
    * Update test to pass with Score D added to score file; update path in readme
    
    * fix requirements.txt after merge
    
    * fix poetry.lock after merge
    
    Co-authored-by: Shelby Switzer <shelby.switzer@cms.hhs.gov>

---
+++ data/data-pipeline/data_pipeline/etl/score/tests/conftest.py
@@ -0,0 +1,118 @@
+import os
+from importlib import reload
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from data_pipeline import config
+from data_pipeline.etl.score import etl_score_post, tests
+from data_pipeline.etl.score.etl_score_post import PostScoreETL
+
+
+def pytest_configure():
+    pytest.SNAPSHOT_DIR = Path(__file__).parent / "snapshots"
+
+
+@pytest.fixture(scope="session")
+def root(tmp_path_factory):
+    basetemp = Path.cwd() / "temp_dir"
+    os.environ["PYTEST_DEBUG_TEMPROOT"] = str(
+        basetemp
+    )  # this sets the location of the temp directory inside the project folder
+    basetemp.mkdir(parents=True, exist_ok=True)
+    root = tmp_path_factory.mktemp("root", numbered=False)
+    return root
+
+
+@pytest.fixture(autouse=True)
+def settings_override(monkeypatch, root):
+    reload(config)
+
+    monkeypatch.setattr(config.settings, "APP_ROOT", root)
+    return config.settings
+
+
+@pytest.fixture()
+def etl(monkeypatch, root):
+    reload(etl_score_post)
+
+    tmp_path = root / "tmp"
+    tmp_path.mkdir(parents=True, exist_ok=True)
+    etl = PostScoreETL()
+    monkeypatch.setattr(etl, "DATA_PATH", root)
+    monkeypatch.setattr(etl, "TMP_PATH", tmp_path)
+    return etl
+
+
+@pytest.fixture(scope="session")
+def sample_data_dir():
+    base_dir = Path(tests.__file__).resolve().parent
+    return base_dir / "sample_data"
+
+
+@pytest.fixture()
+def county_data_initial(sample_data_dir):
+    return sample_data_dir / "county_data_initial.csv"
+
+
+@pytest.fixture()
+def state_data_initial(sample_data_dir):
+    return sample_data_dir / "state_data_initial.csv"
+
+
+@pytest.fixture()
+def score_data_initial(sample_data_dir):
+    return sample_data_dir / "score_data_initial.csv"
+
+
+@pytest.fixture()
+def counties_transformed_expected():
+    return pd.DataFrame.from_dict(
+        data={
+            "State Abbreviation": pd.Series(["AL", "AL"], dtype="string"),
+            "GEOID": pd.Series(["01001", "01003"], dtype="string"),
+            "County Name": pd.Series(
+                ["AutaugaCounty", "BaldwinCounty"], dtype="object"
+            ),
+        },
+    )
+
+
+@pytest.fixture()
+def states_transformed_expected():
+    return pd.DataFrame.from_dict(
+        data={
+            "State Code": pd.Series(["01", "02", "04"], dtype="string"),
+            "State Name": pd.Series(["Alabama", "Alaska", "Arizona"], dtype="object"),
+            "State Abbreviation": pd.Series(["AL", "AK", "AZ"], dtype="string"),
+        },
+    )
+
+
+@pytest.fixture()
+def score_transformed_expected():
+    return pd.read_pickle(pytest.SNAPSHOT_DIR / "score_transformed_expected.pkl")
+
+
+@pytest.fixture()
+def national_cbg_df():
+    return pd.DataFrame.from_dict(
+        data={
+            "GEOID10": pd.Series(["010010201001", "010010201002"], dtype="string"),
+        },
+    )
+
+
+@pytest.fixture()
+def score_data_expected():
+    return pd.read_pickle(pytest.SNAPSHOT_DIR / "score_data_expected.pkl")
+
+
+@pytest.fixture()
+def tile_data_expected():
+    return pd.read_pickle(pytest.SNAPSHOT_DIR / "tile_data_expected.pkl")
+
+
+@pytest.fixture()
+def downloadable_data_expected():
+    return pd.read_pickle(pytest.SNAPSHOT_DIR / "downloadable_data_expected.pkl")
