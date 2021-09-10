fc28680d2edab48d8d432441ace0646769c1f45d
Shelby Switzer <shelby.c.switzer@omb.eop.gov>
2021-09-10 12:10:42 -0400

    Vimusds/hotfix map redeploy (#655)
    * Revert "dockerize front end (#558)"
    
    This reverts commit 89c23faf7acb71e28d2bb3da4408e6346a0fea30.
    
    * trigger redeploy
    
    Co-authored-by: Vim USDS <vimal.k.shah@omb.eop.gov>

--- client/src/components/DatasetContainer/index.tsx
+++ client/src/components/DatasetContainer/index.tsx
@@ -77,6 +77,7 @@ const DatasetContainer = () => {
       <div className={'grid-container-desktop-lg'}>
         <h1 className={styles.datasetContainerHeader}>{intl.formatMessage(messages.cumulativeScore)}</h1>
         <AlertWrapper showBetaAlert={false} showLimitedDataAlert={true}/>
+
         <p className={styles.datasetContainerSubTitle}>{intl.formatMessage(messages.subTitle)}</p>
         <div className={styles.datasetCardsContainer}>
           {cards.map((card) => <DatasetCard
