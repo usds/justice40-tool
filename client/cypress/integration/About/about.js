86e49b30061aed52a29c9b8da765f0fe2ef36931
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-09-13 19:33:56 -0700

    Add gherkin tests for nav to about page
    - navigate from any page to the about page
    - ensure each link has the title correct on that page

--- client/cypress/integration/About/about.js
+++ client/cypress/integration/About/about.js
@@ -1,8 +1,32 @@
 // / <reference types="Cypress" />
-import {Given} from 'cypress-cucumber-preprocessor/steps';
+import {Given, When} from 'cypress-cucumber-preprocessor/steps';
 import {ENDPOINTS} from '../LegacyTests/constants';
 
 // eslint-disable-next-line new-cap
-Given('I open the About page', () => {
+Given('I am on the About page', () => {
+  cy.viewport(1060, 800);
   cy.visit(ENDPOINTS.ABOUT);
+});
+
+// eslint-disable-next-line new-cap
+Given('I am on the Explore Tool page', () => {
+  cy.viewport(1060, 800);
+  cy.visit(ENDPOINTS.EXPLORE_THE_TOOL);
+});
+
+// eslint-disable-next-line new-cap
+Given('I am on the Data & Methodology page', () => {
+  cy.viewport(1060, 800);
+  cy.visit(ENDPOINTS.METHODOLOGY);
+});
+
+// eslint-disable-next-line new-cap
+Given('I open the Contact page', () => {
+  cy.viewport(1060, 800);
+  cy.visit(ENDPOINTS.CONTACT);
+});
+
+// eslint-disable-next-line new-cap
+When(`I click on the {string} page in the navigation`, (page) => {
+  cy.get(`[data-cy="nav-link-${page.toLowerCase()}"]`).click();
 });
--- client/cypress/integration/common/i_see_string_in_the_title.js
+++ client/cypress/integration/common/i_see_string_in_the_title.js
@@ -1,6 +1,4 @@
-import {Then} from 'cypress-cucumber-preprocessor/steps';
-
 // eslint-disable-next-line new-cap
-Then(`I should see {string} in the title`, (title) => {
+Then(`I see {string} in the title`, (title) => {
   cy.title().should('include', title);
 });
