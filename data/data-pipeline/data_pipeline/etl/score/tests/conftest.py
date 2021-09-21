5b2bed2406ad868d8e983395ec1035f1b5e29bf1
Shelby Switzer <shelby.switzer@cms.hhs.gov>
2021-09-21 10:56:52 -0400

    WIP updating test fixtures to include PDF

--- data/data-pipeline/data_pipeline/etl/score/tests/conftest.py
+++ data/data-pipeline/data_pipeline/etl/score/tests/conftest.py
@@ -40,7 +40,9 @@ def etl(monkeypatch, root):
     tmp_path.mkdir(parents=True, exist_ok=True)
     etl = PostScoreETL()
     monkeypatch.setattr(etl, "DATA_PATH", root)
+    monkeypatch.setattr(etl, "FILES_PATH", root)
     monkeypatch.setattr(etl, "TMP_PATH", tmp_path)
+
     return etl
 
 
@@ -63,6 +65,11 @@ def state_data_initial(sample_data_dir):
 @pytest.fixture()
 def score_data_initial(sample_data_dir):
     return sample_data_dir / "score_data_initial.csv"
+
+
+@pytest.fixture()
+def score_pdf_initial(sample_data_dir):
+    return sample_data_dir / "Draft_Communities_List.pdf"
 
 
 @pytest.fixture()
