736fdcb09eede8c38c48b33fa3d1e22b0407e117
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-09-27 12:16:06 -0700

    remove GA script tag and use Gatsby plugin

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -10,6 +10,46 @@ module.exports = {
   pathPrefix: `${process.env.PATH_PREFIX}`,
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-33523145-1',
+
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+
+        // Setting this parameter is optional
+        anonymize: true,
+
+        // Setting this parameter is also optional
+        respectDNT: true,
+
+        // Avoids sending pageview hits from custom paths
+        // exclude: ['/preview/**', '/do-not-track/me/too/'],
+
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+
+        // Enables Google Optimize using your container Id
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Enables Google Optimize Experiment ID
+        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+
+        // Defers execution of google analytics script after page load
+        defer: false,
+
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'screeningtool.geoplatform.gov',
+
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-sass',
       options: {
         sassOptions: {
