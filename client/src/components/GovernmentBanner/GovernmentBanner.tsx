fb93e41b6d62f5a68d3e91741c32db431328520b
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-11-03 15:40:47 -0700

    Add government banner in spanish

--- client/src/components/GovernmentBanner/GovernmentBanner.tsx
+++ client/src/components/GovernmentBanner/GovernmentBanner.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {GovBanner} from '@trussworks/react-uswds';
+import {useIntl} from 'gatsby-plugin-intl';
+
 import Language from '../Language';
 
 import * as styles from './GovernmentBanner.module.scss';
 
 const GovernmentBanner = () => {
+  const intl = useIntl();
   return (
     <div className={styles.fullScreenContainer}>
       <div className={styles.bannerContainer}>
-        <GovBanner/>
+        <GovBanner language={intl.locale === 'es' ? 'spanish' : 'english'}/>
         <Language isDesktop={true}/>
       </div>
     </div>
