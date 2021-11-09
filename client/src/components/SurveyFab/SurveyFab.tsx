9f11951bf67e6d57d74aefd5a13284d7c778eb78
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-11-09 15:22:26 -0800

    Remove embedded survey page
    - add intl

--- client/src/components/SurveyFab/SurveyFab.tsx
+++ client/src/components/SurveyFab/SurveyFab.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import {useIntl} from 'gatsby-plugin-intl';
 import {Button} from '@trussworks/react-uswds';
 
 import * as styles from './SurveyFab.module.scss';
+import * as CONTACT_COPY from '../../data/copy/contact';
 
 const SurveyFab = () => {
+  const intl = useIntl();
   return (
     <Button
       type='button'
       className={styles.surveyFabContainer}
-      onClick={() => Object.assign(document.createElement('a'), {target: '_blank', href: 'https://www.surveymonkey.com/r/cejst-survey'}).click()}
-    >
-      Take our survey!
+      onClick={() => Object.assign(document.createElement('a'), {target: '_blank', href: 'https://www.surveymonkey.com/r/cejst-survey'}).click()}>
+      {intl.formatMessage(CONTACT_COPY.PAGE_INTRO.SURVEY_TEXT)}
     </Button>
   );
 };
