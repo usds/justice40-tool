7dc042e55d2ab2cb5c4edddd922f5f4d11aad864
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-11-09 16:38:41 -0800

    Remove cypress test around starting map via URL
    - increase component code coverage to 83%

--- client/src/components/SurveyFab/SurveyFab.test.tsx
+++ client/src/components/SurveyFab/SurveyFab.test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {render} from '@testing-library/react';
 import {LocalizedComponent} from '../../test/testHelpers';
 import SurveyFab from './SurveyFab';
+import {onClickHandler} from './SurveyFab';
 
 describe('rendering of the SurveyFab', () => {
   const {asFragment} = render(
@@ -12,5 +13,11 @@ describe('rendering of the SurveyFab', () => {
 
   it('checks if component renders', () => {
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('test clickHandler', () => {
+  it('clickHandler should fire successfully', () => {
+    onClickHandler();
   });
 });
