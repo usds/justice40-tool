7efd4e082b37def83a836b6c4e22784ac78c9358
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-11-12 14:25:41 -0800

    Embed surveymonkey form

---
+++ client/src/pages/survey1.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import {Grid} from '@trussworks/react-uswds';
+import {useIntl} from 'gatsby-plugin-intl';
+
+import J40MainGridContainer from '../components/J40MainGridContainer';
+import Layout from '../components/layout';
+
+import * as CONTACT_COPY from '../data/copy/contact';
+
+interface ISurveyPageProps {
+  location: Location;
+}
+
+
+const SurveyPage = ({location}: ISurveyPageProps) => {
+  const intl = useIntl();
+
+  return (
+    <Layout
+      location={location} title={intl.formatMessage(CONTACT_COPY.PAGE_INTRO.SURVEY_TEXT)}>
+      <J40MainGridContainer>
+        <Grid>
+          <Grid col>
+            <iframe
+              src="https://www.surveymonkey.com/r/cejst-survey"
+              width="100%"
+              height="5100px"
+              style={{border: 0}}
+            />
+          </Grid>
+        </Grid>
+      </J40MainGridContainer>
+
+    </Layout>
+  );
+};
+
+export default SurveyPage;
