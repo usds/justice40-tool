9b9c4fb76cf3005a50782932deb95782a95cc848
Jorge Escobar <jorge.e.escobar@omb.eop.gov>
2021-11-23 15:47:46 -0500

    EJScreen Tracts

--- data/data-pipeline/data_pipeline/etl/sources/ejscreen/etl.py
+++ data/data-pipeline/data_pipeline/etl/sources/ejscreen/etl.py
@@ -8,8 +8,8 @@ logger = get_module_logger(__name__)
 
 class EJSCREENETL(ExtractTransformLoad):
     def __init__(self):
-        self.EJSCREEN_FTP_URL = "https://gaftp.epa.gov/EJSCREEN/2019/EJSCREEN_2019_StatePctile.csv.zip"
-        self.EJSCREEN_CSV = self.TMP_PATH / "EJSCREEN_2019_StatePctiles.csv"
+        self.EJSCREEN_FTP_URL = "https://edap-arcgiscloud-data-commons.s3.amazonaws.com/EJSCREEN2020/EJSCREEN_Tract_2020_USPR.csv.zip"
+        self.EJSCREEN_CSV = self.TMP_PATH / "EJSCREEN_Tract_2020_USPR.csv"
         self.CSV_PATH = self.DATA_PATH / "dataset" / "ejscreen_2019"
         self.df: pd.DataFrame
 
