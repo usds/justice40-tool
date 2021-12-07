2843dbe9456e974b845a42244b67a4fbc821af03
Vim USDS <vimal.k.shah@omb.eop.gov>
2021-12-07 09:37:30 -0800

    Add mapbox base layer
    - requires API token

--- client/src/data/mapStyle.tsx
+++ client/src/data/mapStyle.tsx
@@ -82,16 +82,20 @@ const cartoVoyagerBaseLayer = {
 // New "positron" base layer
 const cartoPositronBaseLayer = {
   noLabels: [
-    `https://a.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
-    `https://b.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
-    `https://c.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
-    `https://d.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
+    `https://api.mapbox.com/styles/v1/mapbox/streets-v11.html?title=true&access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA
+    {z}/{x}/{y}${imageSuffix}.png`,
+    // `https://a.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
+    // `https://b.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
+    // `https://c.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
+    // `https://d.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}${imageSuffix}.png`,
   ],
   withLabels: [
-    `https://a.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
-    `https://b.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
-    `https://c.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
-    `https://d.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
+    `https://api.mapbox.com/styles/v1/mapbox/streets-v11.html?title=true&access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA
+    {z}/{x}/{y}${imageSuffix}.png`,
+    // `https://a.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
+    // `https://b.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
+    // `https://c.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
+    // `https://d.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${imageSuffix}.png`,
   ],
 };
 
