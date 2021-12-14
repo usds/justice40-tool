000da0f3ac17444f4e0f2b96b7c2c6adcaf784bf
Vim <86254807+vim-usds@users.noreply.github.com>
2021-12-14 13:11:35 -0500

    Place spanish content on feature ?flags=sp (#1027)
    - update snapshots

--- client/src/components/GovernmentBanner/GovernmentBanner.tsx
+++ client/src/components/GovernmentBanner/GovernmentBanner.tsx
@@ -2,17 +2,32 @@ import React from 'react';
 import {GovBanner} from '@trussworks/react-uswds';
 import {useIntl} from 'gatsby-plugin-intl';
 
+// Contexts:
+import {useFlags} from '../../contexts/FlagContext';
+
 import Language from '../Language';
 
 import * as styles from './GovernmentBanner.module.scss';
 
 const GovernmentBanner = () => {
   const intl = useIntl();
+  const flags = useFlags();
+
   return (
     <div className={styles.fullScreenContainer}>
       <div className={styles.bannerContainer}>
-        <GovBanner language={intl.locale === 'es' ? 'spanish' : 'english'}/>
-        <Language isDesktop={true}/>
+
+        {'sp' in flags ? (
+          <>
+            <GovBanner language={intl.locale === 'es' ? 'spanish' : 'english'}/>
+            <Language isDesktop={true}/>
+          </>
+        ) : (
+          <>
+            <GovBanner/>
+          </>
+      )}
+
       </div>
     </div>
   );
