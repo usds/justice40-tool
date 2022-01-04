bda5d9e6deede0045ec425326cc3ee8309c2d790
Saran Ahluwalia <Saran.S.Ahluwalia@omb.eop.gov>
2022-01-04 14:12:10 -0500

    documentation

--- data/data-pipeline/data_pipeline/etl/sources/cdc_svi_index/etl.py
+++ data/data-pipeline/data_pipeline/etl/sources/cdc_svi_index/etl.py
@@ -10,10 +10,17 @@ logger = get_module_logger(__name__)
 
 
 class CDCSVIINDEX(ExtractTransformLoad):
+    """CDC SVI Index class ingests 2018 dataset located
+    here:
+    Please see the README in this module for further details.
+    """
+
     def __init__(self):
         self.OUTPUT_PATH = self.DATA_PATH / "dataset" / "cdc_svi_index"
 
-        self.CDC_SVI_INDEX_S3_URL = settings.AWS_JUSTICE40_DATASOURCES_URL + "/SVI2018_US.zip"
+        self.CDC_SVI_INDEX_S3_URL = (
+            settings.AWS_JUSTICE40_DATASOURCES_URL + "/SVI2018_US.zip"
+        )
 
         self.CDC_RPL_THEMES_THRSHOLD = 0.90
 
@@ -26,16 +33,17 @@ class CDCSVIINDEX(ExtractTransformLoad):
             "E_TOTPOP",  # Population estimate, 2014-2018 ACS S0601_C01_001 E
             "E_HU",  # Housing units estimate DP04_0001E
             "E_HH",  # Households estimate DP02_0001E
+            # E_PCI has fewer rows than other variables - joined to Census 2016 tracts. Contains null cells (i.e. -999).
             "E_PCI",  # Per capita income estimate, 2014-2018 ACS B19301_001E
             "E_AGE65",  # Persons aged 65 and older estimate, 2014-2018 ACS S0101_C01_030 E
             "E_DISABL",  # Civilian noninstitutionalized population with a disability estimate, 2014 -2018 ACS   DP02_0071E
-            "E_SNGPNT",  # Single parent household with children under 18 estimate MOE, 2014 -2018 ACS DP02_0007E + DP02_0009E
+            "E_SNGPNT",  # Single parent household with children under 18 estimate MOE, 2014 -2018 ACS (DP02_0007E + DP02_0009E)
             "E_CROWD",  # At household level (occupied housing units), more people than rooms estimate, 2014 -2018 ACS (DP04_0078E + DP04_0079E)
         ]
 
         self.COLUMNS_TO_KEEP = [
             self.GEOID_TRACT_FIELD_NAME,
-            field_names.CDC_SVI_INDEX_SE_THEME_PERCENTILE_FIELD,
+            field_names.CDC_SVI_INDEX_SE_THEME_PERCENTILE_FIELD,  # SE prefix references "socioeconomic"
             field_names.CDC_SVI_INDEX_HOUSEHOLD_THEME_PERCENTILE_COMPOSITION_FIELD,
             field_names.CDC_SVI_INDEX_LANGUAGE_THEME_PERCENTILE_FIELD,
             field_names.CDC_SVI_INDEX_HOUSING_TRANSPORTATION_PERCENTILE_FIELD,
@@ -66,8 +74,7 @@ class CDCSVIINDEX(ExtractTransformLoad):
         )
 
         self.df = pd.read_csv(
-            filepath_or_buffer=tmp_svi_csv_file_path,
-            dtype={"FIPS": object}
+            filepath_or_buffer=tmp_svi_csv_file_path, dtype={"FIPS": object}
         )
 
         # Note: In this dataset all US census tracts are ranked against one another.
@@ -92,11 +99,6 @@ class CDCSVIINDEX(ExtractTransformLoad):
             errors="raise",
         )
 
-        # Tracts in the top 10%, i.e., at the 90th percentile of values, are given a value of 1 to indicate high vulnerability.
-        # Tracts below the 90th percentile are given a value of 0.
-        # For a theme, the flag value is the number of flags for variables comprising the theme. Th overall
-        # flag value is calculated for each tract as the number of all variable flags (F_TOTAL)
-
         #  At or above 90 for precentile rank
         self.df[field_names.CDC_SVI_INDEX_THEMES_PRIORITY_COMMUNITY] = (
             self.df[field_names.CDC_RPL_THEMES_OVERALL_PERCENTILE_FIELD]
@@ -114,6 +116,10 @@ class CDCSVIINDEX(ExtractTransformLoad):
     def load(self) -> None:
         logger.info("Saving CDC SVI Index Data")
 
+        # Tracts in the top 10%, i.e., at the 90th percentile of values, are given a value of 1 to indicate high vulnerability.
+        # Tracts below the 90th percentile are given a value of 0.
+        # For a theme, the flag value is the number of flags for variables comprising the theme. Th overall
+        # flag value is calculated for each tract as the number of all variable flags (F_TOTAL)
         list_of_flag_columns = [
             x for x in self.df.columns if str(x).startswith("F_")
         ]
