a8e94ef8de55f818e94e380e6eca0db0a024d7bf
Saran Ahluwalia <Saran.S.Ahluwalia@omb.eop.gov>
2022-01-12 19:06:06 -0500

    updates

--- data/data-pipeline/data_pipeline/etl/sources/maryland_ejscreen/etl.py
+++ data/data-pipeline/data_pipeline/etl/sources/maryland_ejscreen/etl.py
@@ -32,7 +32,8 @@ class MarylandEJScreenETL(ExtractTransformLoad):
             field_names.MARYLAND_EJSCREEN_TRACT_90_PERCENTILE_FIELD,
             field_names.MARYLAND_EJSCREEN_SCORE_FIELD,
             field_names.MARYLAND_EJSCREEN_BURDENED_THRESHOLD_FIELD,
-            field_names.MARYLAND_EJSCREEN_SCORE_FIELD + field_names.PERCENTILE_FIELD_SUFFIX
+            field_names.MARYLAND_EJSCREEN_SCORE_FIELD
+            + field_names.PERCENTILE_FIELD_SUFFIX,
         ]
 
         self.df: pd.DataFrame
@@ -94,13 +95,20 @@ class MarylandEJScreenETL(ExtractTransformLoad):
 
         # Interpretation: An EJ score (reported as a percentile)
         # has a percentile rank of N for some N between 0 - 100"
-        self.df[field_names.MARYLAND_EJSCREEN_SCORE_FIELD + field_names.PERCENTILE_FIELD_SUFFIX] = self.df[
+        self.df[
             field_names.MARYLAND_EJSCREEN_SCORE_FIELD
-        ].rank(pct=True, ascending=True)
+            + field_names.PERCENTILE_FIELD_SUFFIX
+        ] = self.df[field_names.MARYLAND_EJSCREEN_SCORE_FIELD].rank(
+            pct=True, ascending=True
+        )
 
         # An arbitrarily chosen threshold is used in the comparison tool output
         self.df[field_names.MARYLAND_EJSCREEN_BURDENED_THRESHOLD_FIELD] = (
-            self.df[field_names.MARYLAND_EJSCREEN_SCORE_FIELD + field_names.PERCENTILE_FIELD_SUFFIX] > 0.75
+            self.df[
+                field_names.MARYLAND_EJSCREEN_SCORE_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX
+            ]
+            > 0.75
         )
 
         # Baseline Comparisons with some quartiles and the 90th percent OF EJ Score
