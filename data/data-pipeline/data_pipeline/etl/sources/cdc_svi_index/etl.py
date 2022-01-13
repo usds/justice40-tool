f88a4897938d797b62e9d43ebabf2f73525f61d8
Saran Ahluwalia <Saran.S.Ahluwalia@omb.eop.gov>
2022-01-13 17:40:38 -0500

    updates

--- data/data-pipeline/data_pipeline/etl/sources/cdc_svi_index/etl.py
+++ data/data-pipeline/data_pipeline/etl/sources/cdc_svi_index/etl.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from data_pipeline.etl.base import ExtractTransformLoad
 from data_pipeline.utils import get_module_logger
@@ -18,23 +19,24 @@ class CDCSVIIndex(ExtractTransformLoad):
     def __init__(self):
         self.OUTPUT_PATH = self.DATA_PATH / "dataset" / "cdc_svi_index"
 
-        self.CDC_SVI_INDEX_S3_URL = (
-            settings.AWS_JUSTICE40_DATASOURCES_URL + "/SVI2018_US.zip"
-        )
+        self.CDC_SVI_INDEX_URL = "https://svi.cdc.gov/Documents/Data/2018_SVI_Data/CSV/SVI2018_US.csv"
 
         self.CDC_RPL_THEMES_THRESHOLD = 0.90
-
-        self.CDC_SVI_INDEX_ZIP_FILE_DIR = self.TMP_PATH / "cdc_svi_index"
 
         self.CDC_SVI_INDEX_TRACTS_FIPS_CODE = "FIPS"
 
         self.COLUMNS_TO_KEEP = [
             self.GEOID_TRACT_FIELD_NAME,
-            field_names.CDC_SVI_INDEX_SE_THEME_PERCENTILE_FIELD,  # SE prefix references "socioeconomic"
-            field_names.CDC_SVI_INDEX_HOUSEHOLD_THEME_PERCENTILE_COMPOSITION_FIELD,
-            field_names.CDC_SVI_INDEX_LANGUAGE_THEME_PERCENTILE_FIELD,
-            field_names.CDC_SVI_INDEX_HOUSING_TRANSPORTATION_PERCENTILE_FIELD,
-            field_names.CDC_SVI_INDEX_RPL_THEMES_OVERALL_PERCENTILE_FIELD,
+            field_names.CDC_SVI_INDEX_SE_THEME_FIELD
+            + field_names.PERCENTILE_FIELD_SUFFIX,  # SE prefix references "socioeconomic"
+            field_names.CDC_SVI_INDEX_HOUSEHOLD_THEME_COMPOSITION_FIELD
+            + field_names.PERCENTILE_FIELD_SUFFIX,
+            field_names.CDC_SVI_INDEX_LANGUAGE_THEME_FIELD
+            + field_names.PERCENTILE_FIELD_SUFFIX,
+            field_names.CDC_SVI_INDEX_HOUSING_TRANSPORTATION_FIELD
+            + field_names.PERCENTILE_FIELD_SUFFIX,
+            field_names.CDC_SVI_INDEX_RPL_THEMES_OVERALL_FIELD
+            + field_names.PERCENTILE_FIELD_SUFFIX,
             # Derived columns
             field_names.CDC_SVI_INDEX_THEMES_PRIORITY_COMMUNITY,
         ]
@@ -43,43 +45,50 @@ class CDCSVIIndex(ExtractTransformLoad):
 
     def extract(self) -> None:
         logger.info("Extracting 43 MB CDC SVI INDEX")
-        super().extract(
-            self.CDC_SVI_INDEX_S3_URL,
-            self.CDC_SVI_INDEX_ZIP_FILE_DIR,
+        self.df = pd.read_csv(
+            filepath_or_buffer=self.CDC_SVI_INDEX_URL,
+            dtype={self.CDC_SVI_INDEX_TRACTS_FIPS_CODE: "string"},
+            low_memory=False,
         )
 
     def transform(self) -> None:
         logger.info("Starting CDC SVI INDEX transform")
-
-        # New file name
-        tmp_svi_csv_file_path = (
-            self.CDC_SVI_INDEX_ZIP_FILE_DIR / "SVI2018_US.csv"
-        )
-
-        self.df = pd.read_csv(
-            filepath_or_buffer=tmp_svi_csv_file_path,
-            dtype={self.CDC_SVI_INDEX_TRACTS_FIPS_CODE: str},
-        )
-
         # Note: In this dataset all US census tracts are ranked against one another.
         # Puerto Rico is not included in this dataset
         self.df.rename(
             columns={
                 self.CDC_SVI_INDEX_TRACTS_FIPS_CODE: self.GEOID_TRACT_FIELD_NAME,
-                "RPL_THEME1": field_names.CDC_SVI_INDEX_SE_THEME_PERCENTILE_FIELD,
-                "RPL_THEME2": field_names.CDC_SVI_INDEX_HOUSEHOLD_THEME_PERCENTILE_COMPOSITION_FIELD,
-                "RPL_THEME3": field_names.CDC_SVI_INDEX_LANGUAGE_THEME_PERCENTILE_FIELD,
-                "RPL_THEME4": field_names.CDC_SVI_INDEX_HOUSING_TRANSPORTATION_PERCENTILE_FIELD,
-                "RPL_THEMES": field_names.CDC_SVI_INDEX_RPL_THEMES_OVERALL_PERCENTILE_FIELD,
+                "RPL_THEME1": field_names.CDC_SVI_INDEX_SE_THEME_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX,
+                "RPL_THEME2": field_names.CDC_SVI_INDEX_HOUSEHOLD_THEME_COMPOSITION_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX,
+                "RPL_THEME3": field_names.CDC_SVI_INDEX_LANGUAGE_THEME_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX,
+                "RPL_THEME4": field_names.CDC_SVI_INDEX_HOUSING_TRANSPORTATION_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX,
+                "RPL_THEMES": field_names.CDC_SVI_INDEX_RPL_THEMES_OVERALL_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX,
             },
             inplace=True,
             errors="raise",
         )
 
+        # From page 2 in the documentation:
+        # Tracts with zero estimates for total population (N = 645 for the U.S.) were removed during the ranking
+        # process. These tracts were added back to the SVI databases after ranking. The TOTPOP field value is 0,
+        # but the percentile ranking fields (RPL_THEME1, RPL_THEME2, RPL_THEME3, RPL_THEME4, and
+        # RPL_THEMES) were set to -999.
+        # ▪ For tracts with > 0 TOTPOP, a value of -999 in any field either means the value was unavailable from the
+        # original census data or we could not calculate a derived value because of unavailable census data.
+        # ▪ Any cells with a -999 were not used for further calculations. For example, total flags do not include fields
+        # with a -999 value.
+        self.df = self.df.replace(-999, np.nan)
+
         #  At or above 90 for percentile rank
         self.df[field_names.CDC_SVI_INDEX_THEMES_PRIORITY_COMMUNITY] = (
             self.df[
-                field_names.CDC_SVI_INDEX_RPL_THEMES_OVERALL_PERCENTILE_FIELD
+                field_names.CDC_SVI_INDEX_RPL_THEMES_OVERALL_FIELD
+                + field_names.PERCENTILE_FIELD_SUFFIX
             ]
             >= self.CDC_RPL_THEMES_THRESHOLD
         )
@@ -98,17 +107,8 @@ class CDCSVIIndex(ExtractTransformLoad):
     def load(self) -> None:
         logger.info("Saving CDC SVI Index Data")
 
-        # The F-prefixed variables symbolize a flag (as defined in the documentation)
-        # Tracts in the top 10%, i.e., at the 90th percentile of values, are given a value of 1 to indicate high vulnerability.
-        # Tracts below the 90th percentile are given a value of 0.
-        # For a given theme, the flag value is the number of flags for variables comprising the theme. The overall
-        # flag value is calculated for each tract as the number of all variable flags (F_TOTAL)
-        list_of_flag_columns = [
-            x for x in self.df.columns if str(x).startswith("F_")
-        ]
-
         self.OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 
-        self.df[self.COLUMNS_TO_KEEP + list_of_flag_columns].to_csv(
+        self.df[self.COLUMNS_TO_KEEP].to_csv(
             path_or_buf=self.OUTPUT_PATH / "usa.csv", index=False
         )
