2a7ecdcd9b1395b79c80debbe70b7473dcab9c0e
widal001 <williamdaly422@gmail.com>
2022-01-31 21:37:36 -0500

    refactor: Skips tests without update_snapshots when that flag is passed
    Previously when passing the --update_snapshot flag to pytest, all tests 
    would run. This refactor ensures that only the tests with 
    pytest.mark.update_snapshots are run when that flag is passed to the 
    CLI.

--- data/data-pipeline/conftest.py
+++ data/data-pipeline/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def pytest_addoption(parser):
-    """Adds --update_snapshots as potential flag that can be parsed from the
+    """Adds --update_snapshots as an optional flag that can be parsed from the
     command line when calling pytest
     """
     parser.addoption(
@@ -23,13 +23,18 @@ def pytest_configure(config):
 
 def pytest_collection_modifyitems(config, items):
     """Applies pytest.mark.skip to all tests marked with update_snapshots
-    unless the --update_snapshots is passed to the CLI
+    unless the --update_snapshots flag is passed to the CLI
     """
-    # if --update_snapshots passed in cli: do not skip slow tests
+    # if --update_snapshots passed in cli
     if config.getoption("--update_snapshots"):
-        return
-    # otherwise skip all tests marked with update_snapshots
-    skip = pytest.mark.skip(reason="needs --update_snapshots option to run")
-    for item in items:
-        if "update_snapshots" in item.keywords:
-            item.add_marker(skip)
+        # only run the tests marked with update_snapshots
+        skip = pytest.mark.skip(reason="only updating snapshots")
+        for test in items:
+            if "update_snapshots" not in test.keywords:
+                test.add_marker(skip)
+    else:
+        # otherwise skip all tests marked with update_snapshots
+        skip = pytest.mark.skip(reason="must pass --update_snapshots to run")
+        for test in items:
+            if "update_snapshots" in test.keywords:
+                test.add_marker(skip)
