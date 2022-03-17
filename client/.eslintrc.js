96c61b9d11c793ba620504ba3bc1a1e9c1f522bd
Nat Hillard <Nathaniel.K.Hillard@omb.eop.gov>
2022-03-17 16:48:02 -0400

    fixing formatting

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'plugin:cypress/recommended',
-    'google',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-    'formatjs',
-  ],
-  'rules': {
-    'max-len': [2, {'code': 120, 'tabWidth': 4, 'ignoreUrls': true}],
-    'formatjs/no-offset': 'error',
-    'formatjs/enforce-description': 'error',
-    'formatjs/enforce-default-message': 'error',
-    'formatjs/enforce-placeholders': 'error',
-  },
-  'settings': {
-    'react': {
-      'version': 'detect',
+    'extends': [
+        'plugin:react/recommended',
+        'plugin:cypress/recommended',
+        'google',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module',
     },
-  },
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'formatjs',
+    ],
+    'rules': {
+        'max-len': [2, { 'code': 120, 'tabWidth': 4, 'ignoreUrls': true }],
+        'formatjs/no-offset': 'error',
+        'formatjs/enforce-description': 'error',
+        'formatjs/enforce-default-message': 'error',
+        'formatjs/enforce-placeholders': 'error',
+    },
+    'settings': {
+        'react': {
+            'version': 'detect',
+        },
+    },
 };
