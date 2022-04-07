34ba3bbda9d98cc819916e67035e47262ed4ca79
Vim USDS <vimal.k.shah@omb.eop.gov>
2022-04-07 14:30:49 -0700

    Add script to un-nest the translated es.json file

---
+++ client/src/intl/removeNesting.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const englishJson = require('./es_translated_final.json');
+
+// placeholder for fixed es json file
+const fixedEs = {};
+
+// loop through incorrect es.json file modify
+for (const [key, value] of Object.entries(englishJson)) {
+  fixedEs[key] = value.defaultMessage;
+};
+
+console.log(fixedEs);
+
+// convert JSON object to string
+const fixedEsString = JSON.stringify(fixedEs);
+
+// write JSON string to a file
+fs.writeFile('esNoNesting.json', fixedEsString, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('JSON data is saved.');
+});
+
+
