eb1931cf7a081e99b18d064eae242f0f83679143
Jorge Escobar <83969469+esfoobar-usds@users.noreply.github.com>
2022-04-25 18:59:58 -0400

    Updates to Spanish Site (#1585)
    * Updates to Spanish Site
    
    * update bad edit
    
    * take feature flag out
    
    * removing feature flag from Language component
    
    * bad return
    
    * updated snapshots
    
    * Update unit tests
    
    Co-authored-by: TomNUSDS <tomn.usds.opensource+tomnusds@gmail.com>

--- client/src/components/GovernmentBanner/GovernmentBanner.tsx
+++ client/src/components/GovernmentBanner/GovernmentBanner.tsx
@@ -2,31 +2,19 @@ import React from 'react';
 import {GovBanner} from '@trussworks/react-uswds';
 import {useIntl} from 'gatsby-plugin-intl';
 
-// Contexts:
-import {useFlags} from '../../contexts/FlagContext';
-
 import Language from '../Language';
 
 import * as styles from './GovernmentBanner.module.scss';
 
 const GovernmentBanner = () => {
   const intl = useIntl();
-  const flags = useFlags();
 
   return (
     <div className={styles.fullScreenContainer}>
       <div className={styles.bannerContainer}>
 
-        {'sp' in flags ? (
-          <>
-            <GovBanner language={intl.locale === 'es' ? 'spanish' : 'english'}/>
-            <Language isDesktop={true}/>
-          </>
-        ) : (
-          <>
-            <GovBanner/>
-          </>
-      )}
+        <GovBanner language={intl.locale === 'es' ? 'spanish' : 'english'}/>
+        <Language isDesktop={true}/>
 
       </div>
     </div>
