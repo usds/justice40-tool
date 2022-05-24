c5be2742c058e03ae3f554574cb8f7fc988dad53
Vim USDS <vimal.k.shah@omb.eop.gov>
2022-05-24 16:34:53 -0700

    Make the 3rd bullet on explore page update on 5/25

--- client/src/components/HowYouCanHelp/index.tsx
+++ client/src/components/HowYouCanHelp/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import * as styles from './howYouCanHelp.module.scss';
 import * as EXPLORE_COPY from '../../data/copy/explore';
+import * as COMMON_COPY from '../../data/copy/common';
 
 const HowYouCanHelp = () => {
+  const isRfiValid = new Date() < COMMON_COPY.ALERTS.EXPIRATION_DATE;
+
   return (
     <div className={styles.howYouCanHelpContainer}>
       <h2>
@@ -16,9 +19,15 @@ const HowYouCanHelp = () => {
         <li className={styles.howYouCanHelpList}>
           {EXPLORE_COPY.HOW_YOU_CAN_HELP_LIST_ITEMS.LIST_ITEM_2}
         </li>
+        {
+          isRfiValid ?
         <li className={styles.howYouCanHelpList}>
           {EXPLORE_COPY.HOW_YOU_CAN_HELP_LIST_ITEMS.LIST_ITEM_3}
+        </li> :
+        <li className={styles.howYouCanHelpList}>
+          {EXPLORE_COPY.HOW_YOU_CAN_HELP_LIST_ITEMS.LIST_ITEM_4}
         </li>
+        }
       </ul>
     </div>
   );
