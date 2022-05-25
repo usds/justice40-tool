ef46af00f89f8ad6c6624526b9fb3f4c18b5b970
Vim <86254807+vim-usds@users.noreply.github.com>
2022-05-25 12:10:14 -0400

    Add time to expiration date (#1654)
    * Push out expiration by a day for fast fix
    
    * Add timestampt to expiration
    
    - update snapshots
    
    * Remove timed alerts

--- client/src/components/RequestForInfo/RequestForInfo.tsx
+++ client/src/components/RequestForInfo/RequestForInfo.tsx
@@ -8,12 +8,11 @@ import * as COMMON_COPY from '../../data/copy/common';
 
 const RequestForInfo = () => {
   const intl = useIntl();
-  const commentPeriodExpired = new Date < COMMON_COPY.ALERTS.EXPIRATION_DATE;
 
   return (
     <SummaryBox className={styles.rfiBox} heading={intl.formatMessage(CONTACT_COPY.RFI_BOX.TITLE)}>
       <p>{CONTACT_COPY.RFI_BOX_BODY}</p>
-      <p>{commentPeriodExpired && COMMON_COPY.ALERTS.ALERT_1_DESCRIPTION}</p>
+      <p>{COMMON_COPY.ALERTS.ALERT_1_DESCRIPTION}</p>
     </SummaryBox>
   );
 };
