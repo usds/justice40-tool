e571f80008a92162800c8c69843d06c8ffbad36b
Vim USDS <vimal.k.shah@omb.eop.gov>
2022-07-01 10:50:37 -0700

    Add CSP plugin
    - OWASP flagged a minor issue with CSP. Checking if this will fix the issue

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -105,5 +105,21 @@ module.exports = {
         sampleRate: 0.7,
       },
     },
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          'script-src': '\'self\' www.google-analytics.com',
+          'style-src': '\'self\' \'unsafe-inline\'',
+          'img-src': '\'self\' data: www.google-analytics.com',
+          // you can add your directives or override defaults
+        },
+      },
+    },
   ],
 };
