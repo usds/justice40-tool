b2f64fb851c933956fd97a96af7dd008e96ae07f
Vim USDS <vimal.k.shah@omb.eop.gov>
2022-07-14 16:16:36 -0700

    Change commented code to block comment

--- client/.generate_component/index.js
+++ client/.generate_component/index.js
@@ -33,22 +33,23 @@ fs.writeFile(`${dir}/index.ts`, barrel(name), writeFileErrorHandler);
 ////////////////
 
 // insert new component into 'components/index.ts file
-// fs.readFile('./src/components/index.ts', 'utf8', function(err, data) {
-//   if (err) throw err;
 
-//   // grab all components and combine them with new component
-//   const currentComponents = data.match(/(?<=import )(.*?)(?= from)/g);
-//   const newComponents = [name, ...currentComponents].sort();
+/* fs.readFile('./src/components/index.ts', 'utf8', function(err, data) {
+  if (err) throw err;
 
-//   // create the import and export statements
-//   const importStatements = newComponents
-//     .map(importName => `import ${importName} from './${importName}';\n`)
-//     .join('');
-//   const exportStatements = `export {\n${newComponents
-//     .map(component => `  ${component},\n`)
-//     .join('')}};\n`;
+  // grab all components and combine them with new component
+  const currentComponents = data.match(/(?<=import )(.*?)(?= from)/g);
+  const newComponents = [name, ...currentComponents].sort();
 
-//   const fileContent = `${importStatements}\n${exportStatements}`;
+  // create the import and export statements
+  const importStatements = newComponents
+    .map(importName => `import ${importName} from './${importName}';\n`)
+    .join('');
+  const exportStatements = `export {\n${newComponents
+    .map(component => `  ${component},\n`)
+    .join('')}};\n`;
 
-//   fs.writeFile(`./src/components/index.ts`, fileContent, writeFileErrorHandler);
-// });
+  const fileContent = `${importStatements}\n${exportStatements}`;
+
+  fs.writeFile(`./src/components/index.ts`, fileContent, writeFileErrorHandler);
+}); */
