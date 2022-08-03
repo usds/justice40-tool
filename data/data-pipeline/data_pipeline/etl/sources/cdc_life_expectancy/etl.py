c6a7a28ca63776335f323c069530f2ea110d8d5d
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-08-03 15:35:31 -0400

    Refactor CDC life-expectancy (1554)

--- data/data-pipeline/data_pipeline/etl/sources/cdc_life_expectancy/etl.py
+++ data/data-pipeline/data_pipeline/etl/sources/cdc_life_expectancy/etl.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 import pandas as pd
 
-from data_pipeline.etl.base import ExtractTransformLoad
-from data_pipeline.utils import get_module_logger, download_file_from_url
+from data_pipeline.etl.base import ExtractTransformLoad, ValidGeoLevel
+from data_pipeline.utils import download_file_from_url, get_module_logger
 
 logger = get_module_logger(__name__)
 
 
 class CDCLifeExpectancy(ExtractTransformLoad):
+    NAME = "cdc_life_expectancy"
+    GEO_LEVEL: ValidGeoLevel = ValidGeoLevel.CENSUS_TRACT
+
     def __init__(self):
         self.FILE_URL: str = "https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NVSS/USALEEP/CSV/US_A.CSV"
         self.OUTPUT_PATH: Path = (
             self.DATA_PATH / "dataset" / "cdc_life_expectancy"
         )
 
-        self.TRACT_INPUT_COLUMN_NAME = "Tract ID"
         self.LIFE_EXPECTANCY_FIELD_NAME = "Life expectancy (years)"
+        self._DOWNLOAD_FILE_LOCATION = self.get_tmp_path() / "US_A.CSV"
 
         # Constants for output
         self.COLUMNS_TO_KEEP = [
@@ -23,37 +26,34 @@ class CDCLifeExpectancy(ExtractTransformLoad):
             self.LIFE_EXPECTANCY_FIELD_NAME,
         ]
 
-        self.raw_df: pd.DataFrame
         self.output_df: pd.DataFrame
 
     def extract(self) -> None:
+        # Needs to be overridden because the data aren't zipped
         logger.info("Starting data download.")
 
-        download_file_name = (
-            self.get_tmp_path() / "cdc_life_expectancy" / "usa.csv"
-        )
         download_file_from_url(
             file_url=self.FILE_URL,
-            download_file_name=download_file_name,
+            download_file_name=self._DOWNLOAD_FILE_LOCATION,
             verify=True,
-        )
-
-        self.raw_df = pd.read_csv(
-            filepath_or_buffer=download_file_name,
-            dtype={
-                # The following need to remain as strings for all of their digits, not get converted to numbers.
-                self.TRACT_INPUT_COLUMN_NAME: "string",
-            },
-            low_memory=False,
         )
 
     def transform(self) -> None:
         logger.info("Starting DOE energy burden transform.")
 
-        self.output_df = self.raw_df.rename(
+        raw_df = pd.read_csv(
+            filepath_or_buffer=self._DOWNLOAD_FILE_LOCATION,
+            dtype={
+                # The following need to remain as strings for all of their digits, not get converted to numbers.
+                self.INPUT_GEOID_TRACT_FIELD_NAME: "string",
+            },
+            low_memory=False,
+        )
+
+        self.output_df = raw_df.rename(
             columns={
                 "e(0)": self.LIFE_EXPECTANCY_FIELD_NAME,
-                self.TRACT_INPUT_COLUMN_NAME: self.GEOID_TRACT_FIELD_NAME,
+                self.INPUT_GEOID_TRACT_FIELD_NAME: self.GEOID_TRACT_FIELD_NAME,
             }
         )
 
