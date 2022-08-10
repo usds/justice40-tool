d0a31faf4c6e495feaea7fa1855cf64ab19ee233
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-08-10 17:59:54 -0400

    Add FUDS ETL, tests, test-datae noteobook (#1871)
    This adds the ETL class for Formerly Used Defense Sites (FUDS). This is
    different from most other ETLs since these FUDS are not provided by
    tract, but instead by geographic point, so we need to assign FUDS to
    tracts and then do calculations from there.

---
+++ data/data-pipeline/data_pipeline/etl/sources/us_army_fuds/etl.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+import geopandas as gpd
+import pandas as pd
+
+from data_pipeline.etl.base import ExtractTransformLoad, ValidGeoLevel
+from data_pipeline.utils import get_module_logger, download_file_from_url
+from data_pipeline.etl.sources.geo_utils import add_tracts_for_geometries
+
+logger = get_module_logger(__name__)
+
+
+class USArmyFUDS(ExtractTransformLoad):
+    """The Formerly Used Defense Sites (FUDS)"""
+
+    NAME: str = "us_army_fuds"
+
+    ELIGIBLE_FUDS_COUNT_FIELD_NAME: str
+    INELIGIBLE_FUDS_COUNT_FIELD_NAME: str
+    ELIGIBLE_FUDS_BINARY_FIELD_NAME: str
+    GEO_LEVEL: ValidGeoLevel = ValidGeoLevel.CENSUS_TRACT
+
+    def __init__(self):
+        self.FILE_URL: str = (
+            "https://opendata.arcgis.com/api/v3/datasets/"
+            "3f8354667d5b4b1b8ad7a6e00c3cf3b1_1/downloads/"
+            "data?format=geojson&spatialRefId=4326&where=1%3D1"
+        )
+
+        self.OUTPUT_PATH: Path = self.DATA_PATH / "dataset" / "us_army_fuds"
+
+        # Constants for output
+        self.COLUMNS_TO_KEEP = [
+            self.GEOID_TRACT_FIELD_NAME,
+            self.ELIGIBLE_FUDS_COUNT_FIELD_NAME,
+            self.INELIGIBLE_FUDS_COUNT_FIELD_NAME,
+            self.ELIGIBLE_FUDS_BINARY_FIELD_NAME,
+        ]
+        self.DOWNLOAD_FILE_NAME = self.get_tmp_path() / "fuds.geojson"
+
+        self.raw_df: gpd.GeoDataFrame
+        self.output_df: pd.DataFrame
+
+    def extract(self) -> None:
+        logger.info("Starting data download.")
+
+        download_file_from_url(
+            file_url=self.FILE_URL,
+            download_file_name=self.DOWNLOAD_FILE_NAME,
+            verify=True,
+        )
+
+    def transform(self) -> None:
+        logger.info("Starting FUDS transform.")
+
+        raw_df = gpd.read_file(
+            filename=self.DOWNLOAD_FILE_NAME,
+            low_memory=False,
+        )
+
+        # Note that the length of raw_df will not be exactly the same
+        # because same bases lack coordinated or have coordinates in
+        # Mexico or in the ocean. See the following dataframe:
+        # raw_df[~raw_df.OBJECTID.isin(df_with_tracts.OBJECTID)][
+        # ['OBJECTID', 'CLOSESTCITY', 'COUNTY', 'ELIGIBILITY',
+        # 'STATE', 'LATITUDE', "LONGITUDE"]]
+        df_with_tracts = add_tracts_for_geometries(raw_df)
+
+        # Now, do some counting
+        # XXX: @emma-nechamkin do the conditions for has_eligible_projects and
+        # has_ineligble_projects look like what you were thinking?
+        df_with_tracts.loc[
+            (
+                (df_with_tracts.ELIGIBILITY == "Eligible")
+                & (df_with_tracts.HASPROJECTS == "Yes")
+            ),
+            "has_eligible_projects",
+        ] = 1
+        df_with_tracts.has_eligible_projects.fillna(0, inplace=True)
+        df_with_tracts.loc[
+            (
+                (df_with_tracts.ELIGIBILITY == "Inligible")
+                | (df_with_tracts.HASPROJECTS == "No")
+                | (df_with_tracts.ELIGIBILITY.isna())
+                | (df_with_tracts.HASPROJECTS.isna())
+            ),
+            "has_ineligible_projects",
+        ] = 1
+        df_with_tracts.has_ineligible_projects.fillna(0, inplace=True)
+        self.output_df = (
+            df_with_tracts.groupby("GEOID10_TRACT")[
+                ["has_eligible_projects", "has_ineligible_projects"]
+            ]
+            .sum()
+            .rename(
+                columns={
+                    "has_eligible_projects": self.ELIGIBLE_FUDS_COUNT_FIELD_NAME,
+                    "has_ineligible_projects": self.INELIGIBLE_FUDS_COUNT_FIELD_NAME,
+                }
+            )
+        ).reset_index()
+        self.output_df[self.ELIGIBLE_FUDS_BINARY_FIELD_NAME] = (
+            self.output_df[self.ELIGIBLE_FUDS_COUNT_FIELD_NAME] > 0.0
+        )
---
+++ data/data-pipeline/data_pipeline/tests/sources/us_army_fuds/test_etl.py
@@ -0,0 +1,171 @@
+# pylint: disable=protected-access
+import unittest.mock as mock
+import pathlib
+from data_pipeline.etl.base import ValidGeoLevel
+
+from data_pipeline.etl.sources.us_army_fuds.etl import (
+    USArmyFUDS,
+)
+from data_pipeline.tests.sources.example.test_etl import TestETL
+from data_pipeline.utils import get_module_logger
+
+logger = get_module_logger(__name__)
+
+
+def _fake_add_tracts_for_geometries(df):
+    """The actual geojoin is too slow for tests. Use procomputed results."""
+    lookups = {
+        (-121.39361572299998, 38.87463378900003): "06061021322",
+        (-121.40020751999998, 38.897583008000026): "06061021322",
+        (-121.40020751999998, 38.75158691400003): "06061021322",
+        (-157.84301757799997, 21.53619384800004): "15003010201",
+        (-157.85168456999997, 21.553405762000068): "15003010201",
+        (-157.90679931599996, 21.554199219000054): "15003010201",
+        (-159.52191162099996, 21.976623535000044): "15007040700",
+        (-159.52996826199998, 21.93762207000003): "15007040700",
+        (-159.52111816399997, 21.922607422000056): "15007040700",
+        (-156.14270019499997, 20.840393066000047): "15009030100",
+        (-155.85968017599998, 20.26519775400004): "15001021800",
+        (-155.73327636699997, 20.166809082000043): "15001021800",
+        (-155.89270019499997, 20.23522949200003): "15001021800",
+        (-156.26019287099996, 20.899414062000062): "15009030201",
+        (-156.22076415999996, 20.91241455100004): "15009030201",
+        (-156.20739746099997, 20.890991211000028): "15009030201",
+        (-159.46496581999997, 21.90460205100004): "15007040603",
+        (-159.46441650399998, 21.905212402000075): "15007040603",
+        (-154.82519531299997, 19.49182128900003): "15001021101",
+        (-121.06768798799999, 36.61480712900004): "06069000802",
+        (-117.391601563, 36.33343505900007): "06027000800",
+        (-117.85546874999994, 36.46960449200003): "06027000800",
+        (-117.23529052699996, 36.387634277000075): "06027000800",
+        (-118.15270996099997, 36.725219727000024): "06027000800",
+        (-118.13891601599994, 36.56683349600007): "06027000800",
+        (-117.311096191, 36.783386230000076): "06027000800",
+        (-118.00030517599998, 36.283813477000024): "06027000800",
+        (-116.86248779299996, 36.46124267600004): "06027000800",
+        (-117.16418456999997, 36.60681152300003): "06027000800",
+        (-117.06939697299998, 36.158386230000076): "06027000800",
+        (-117.873596191, 36.487609863000046): "06027000800",
+        (-116.82971191399997, 36.283386230000076): "06027000800",
+        (-117.21667480499997, 35.95843505900007): "06027000800",
+        (-118.04998779299996, 36.59478759800004): "06027000800",
+        (-117.03576660199997, 36.27801513700007): "06027000800",
+        (-116.10028076199995, 35.83380127000004): "06027000800",
+        (-117.86499023399995, 36.14422607400007): "06027000800",
+        (-155.10320912843935, 19.497857096442765): "15001021010",
+        (-155.91378674587037, 19.516632121497878): "15001021402",
+        (-156.3306524489697, 20.825377142028497): "15009030402",
+        (-156.5429023670438, 20.917074254751412): "15009030800",
+        (-159.48416820625405, 21.907546119100093): "15007040604",
+    }
+    df["GEOID10_TRACT"] = df.geometry.apply(
+        lambda point: lookups[(point.x, point.y)]
+    )
+    return df
+
+
+class TestUSArmyFUDSETL(TestETL):
+    """Tests the FUDS ETL.
+
+    This uses pytest-snapshot.
+    To update individual snapshots: $ poetry run pytest
+            data_pipeline/tests/sources/us_army_fuds/test_etl.py::TestClassNameETL::<testname>
+            --snapshot-update
+    """
+
+    _ETL_CLASS = USArmyFUDS
+
+    _SAMPLE_DATA_PATH = pathlib.Path(__file__).parents[0] / "data"
+    _SAMPLE_DATA_FILE_NAME = "fuds.geojson"
+    _SAMPLE_DATA_ZIP_FILE_NAME = "fuds.geojson"
+    _EXTRACT_TMP_FOLDER_NAME = "USArmyFUDS"
+
+    def setup_method(self, _method, filename=__file__):
+        """Invoke `setup_method` from Parent, but using the current file name.
+
+        This code can be copied identically between all child classes.
+        """
+        super().setup_method(_method=_method, filename=filename)
+
+    def test_init(self, mock_etl, mock_paths):
+        """Tests that the mock NationalRiskIndexETL class instance was
+        initiliazed correctly.
+
+        Validates the following conditions:
+        - self.DATA_PATH points to the "data" folder in the temp directory
+        - self.TMP_PATH points to the "data/tmp" folder in the temp directory
+        - self.INPUT_PATH points to the correct path in the temp directory
+        - self.OUTPUT_PATH points to the correct path in the temp directory
+        """
+        # setup
+        etl = self._ETL_CLASS()
+        # validation
+        assert etl.GEOID_FIELD_NAME == "GEOID10"
+        assert etl.GEOID_TRACT_FIELD_NAME == "GEOID10_TRACT"
+        assert etl.NAME == "us_army_fuds"
+        assert etl.GEO_LEVEL == ValidGeoLevel.CENSUS_TRACT
+        assert etl.COLUMNS_TO_KEEP == [
+            etl.GEOID_TRACT_FIELD_NAME,
+            etl.ELIGIBLE_FUDS_COUNT_FIELD_NAME,
+            etl.INELIGIBLE_FUDS_COUNT_FIELD_NAME,
+            etl.ELIGIBLE_FUDS_BINARY_FIELD_NAME,
+        ]
+
+    def test_get_output_file_path(self, mock_etl, mock_paths):
+        """Tests the right file name is returned."""
+        etl = self._ETL_CLASS()
+        data_path, tmp_path = mock_paths
+
+        output_file_path = etl._get_output_file_path()
+        expected_output_file_path = (
+            data_path / "dataset" / self._ETL_CLASS.NAME / "usa.csv"
+        )
+        assert output_file_path == expected_output_file_path
+
+    def test_fixtures_contain_shared_tract_ids_base(self, mock_etl, mock_paths):
+        with mock.patch(
+            "data_pipeline.etl.sources.us_army_fuds.etl.add_tracts_for_geometries",
+            new=_fake_add_tracts_for_geometries,
+        ):
+            return super().test_fixtures_contain_shared_tract_ids_base(
+                mock_etl, mock_paths
+            )
+
+    def test_transform_base(self, snapshot, mock_etl, mock_paths):
+        with mock.patch(
+            "data_pipeline.etl.sources.us_army_fuds.etl.add_tracts_for_geometries",
+            new=_fake_add_tracts_for_geometries,
+        ):
+            super().test_transform_base(
+                snapshot=snapshot, mock_etl=mock_etl, mock_paths=mock_paths
+            )
+
+    def test_transform_sets_output_df_base(self, mock_etl, mock_paths):
+        with mock.patch(
+            "data_pipeline.etl.sources.us_army_fuds.etl.add_tracts_for_geometries",
+            new=_fake_add_tracts_for_geometries,
+        ):
+            super().test_transform_sets_output_df_base(
+                mock_etl=mock_etl, mock_paths=mock_paths
+            )
+
+    def test_validate_base(self, mock_etl, mock_paths):
+        with mock.patch(
+            "data_pipeline.etl.sources.us_army_fuds.etl.add_tracts_for_geometries",
+            new=_fake_add_tracts_for_geometries,
+        ):
+            super().test_validate_base(mock_etl=mock_etl, mock_paths=mock_paths)
+
+    def test_full_etl_base(self, mock_etl, mock_paths):
+        with mock.patch(
+            "data_pipeline.etl.sources.us_army_fuds.etl.add_tracts_for_geometries",
+            new=_fake_add_tracts_for_geometries,
+        ):
+            return super().test_full_etl_base(mock_etl, mock_paths)
+
+    def test_get_data_frame_base(self, mock_etl, mock_paths):
+        with mock.patch(
+            "data_pipeline.etl.sources.us_army_fuds.etl.add_tracts_for_geometries",
+            new=_fake_add_tracts_for_geometries,
+        ):
+            return super().test_get_data_frame_base(mock_etl, mock_paths)
