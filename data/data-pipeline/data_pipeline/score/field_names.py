3b4ad8c7aaa931ecbdb2d3e6cd350d1b58ba888b
lucasmbrown-usds <lucas.m.brown@omb.eop.gov>
2022-08-18 12:27:08 -0400

    updating abbreviations

--- data/data-pipeline/data_pipeline/score/field_names.py
+++ data/data-pipeline/data_pipeline/score/field_names.py
@@ -100,14 +100,14 @@ LOW_MEDIAN_INCOME_AS_PERCENT_OF_AMI_FIELD = (
 )
 
 # Additional ACS demographic fields.
-BLACK_FIELD_NAME = "Black or African Amer."
-AMERICAN_INDIAN_FIELD_NAME = "Amer. Indian / Alaska Native"
+BLACK_FIELD_NAME = "Black or African American"
+AMERICAN_INDIAN_FIELD_NAME = "American Indian / Alaska Native"
 ASIAN_FIELD_NAME = "Asian"
 HAWAIIAN_FIELD_NAME = "Native Hawaiian or Pacific"
-TWO_OR_MORE_RACES_FIELD_NAME = "Two or more"
+TWO_OR_MORE_RACES_FIELD_NAME = "Two or more races"
 NON_HISPANIC_WHITE_FIELD_NAME = "White"
 HISPANIC_FIELD_NAME = "Hispanic or Latino"
-OTHER_RACE_FIELD_NAME = "Other"
+OTHER_RACE_FIELD_NAME = "Other Races"
 
 # Same fields, but with a percent prefix
 PERCENT_PREFIX = "Percent "
