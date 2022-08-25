34da5cdd15d8cf505330b2878c3845bdbc833738
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-08-25 17:42:04 -0400

    Switch from lru_cache to fixture scorpes (#1846)

--- data/data-pipeline/data_pipeline/tests/score/fixtures.py
+++ data/data-pipeline/data_pipeline/tests/score/fixtures.py
@@ -1,12 +1,10 @@
-from functools import lru_cache
 import pandas as pd
 import pytest
 from data_pipeline.config import settings
 import data_pipeline.score.field_names as field_names
 
 
-@lru_cache
-@pytest.fixture
+@pytest.fixture(scope="session")
 def final_score_df():
     return pd.read_csv(
         settings.APP_ROOT / "data" / "score" / "csv" / "full" / "usa.csv",
