39fa5dd012d4c79602b1c4aa269dfc61f0713a76
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-08-25 13:29:19 -0400

    Refactor output tests like calculatoin tests (#1846)

--- data/data-pipeline/data_pipeline/tests/test_score_output.py
+++ data/data-pipeline/data_pipeline/tests/test_score_output.py
@@ -1,127 +1,9 @@
 # flake8: noqa: W0613,W0611,F811
-import pytest
+from dataclasses import dataclass
+from typing import List
+import pandas as pd
 import data_pipeline.score.field_names as field_names
-from data_pipeline.utils import get_module_logger
-from data_pipeline.score.score_narwhal import ScoreNarwhal
 from .score_fixtures import final_score_df  # pylint: disable=unused-import
-
-
-logger = get_module_logger(__name__)
-
-
-# when should things be fixtures??
-@pytest.fixture
-def climate_thresholds():
-    return [
-        field_names.EXPECTED_POPULATION_LOSS_EXCEEDS_PCTILE_THRESHOLD,
-        field_names.EXPECTED_AGRICULTURAL_LOSS_EXCEEDS_PCTILE_THRESHOLD,
-        field_names.EXPECTED_BUILDING_LOSS_EXCEEDS_PCTILE_THRESHOLD,
-        field_names.HIGH_FUTURE_FLOOD_RISK_FIELD,
-        field_names.HIGH_FUTURE_WILDFIRE_RISK_FIELD,
-    ]
-
-
-@pytest.fixture
-def environmental_burden_threshold():
-    return ScoreNarwhal.ENVIRONMENTAL_BURDEN_THRESHOLD
-
-
-@pytest.fixture
-def energy_thresholds():
-    return [
-        field_names.ENERGY_BURDEN_EXCEEDS_PCTILE_THRESHOLD,
-        field_names.PM25_EXCEEDS_PCTILE_THRESHOLD,
-    ]
-
-
-@pytest.fixture
-def transportation_thresholds():
-    return [
-        field_names.DIESEL_EXCEEDS_PCTILE_THRESHOLD,
-        field_names.DOT_BURDEN_PCTILE_THRESHOLD,
-        field_names.TRAFFIC_PROXIMITY_PCTILE_THRESHOLD,
-    ]
-
-
-@pytest.fixture
-def housing_thresholds():
-    return [
-        field_names.HISTORIC_REDLINING_SCORE_EXCEEDED,
-        field_names.NO_KITCHEN_OR_INDOOR_PLUMBING_PCTILE_THRESHOLD,
-        field_names.LEAD_PAINT_PROXY_PCTILE_THRESHOLD,
-        field_names.HOUSING_BURDEN_PCTILE_THRESHOLD,
-        field_names.NON_NATURAL_PCTILE_THRESHOLD,
-    ]
-
-
-@pytest.fixture
-def pollution_thresholds():
-    return [
-        field_names.RMP_PCTILE_THRESHOLD,
-        field_names.NPL_PCTILE_THRESHOLD,
-        field_names.TSDF_PCTILE_THRESHOLD,
-        field_names.AML_BOOLEAN,
-        field_names.ELIGIBLE_FUDS_BINARY_FIELD_NAME,
-    ]
-
-
-@pytest.fixture
-def water_thresholds():
-    return [
-        field_names.WASTEWATER_PCTILE_THRESHOLD,
-        field_names.UST_PCTILE_THRESHOLD,
-    ]
-
-
-@pytest.fixture
-def health_thresholds():
-    return [
-        field_names.DIABETES_PCTILE_THRESHOLD,
-        field_names.ASTHMA_PCTILE_THRESHOLD,
-        field_names.HEART_DISEASE_PCTILE_THRESHOLD,
-        field_names.LOW_LIFE_EXPECTANCY_PCTILE_THRESHOLD,
-    ]
-
-
-## TODO: Island Area columns?
-@pytest.fixture
-def workforce_base_thresholds():
-    return [
-        field_names.UNEMPLOYMENT_PCTILE_THRESHOLD,
-        field_names.LOW_MEDIAN_INCOME_PCTILE_THRESHOLD,
-        field_names.LINGUISTIC_ISOLATION_PCTILE_THRESHOLD,
-        field_names.POVERTY_PCTILE_THRESHOLD,
-    ]
-
-
-@pytest.fixture
-def low_hs_ses():
-    return field_names.LOW_HS_EDUCATION_FIELD
-
-
-@pytest.fixture
-def low_inc_ses():
-    return field_names.FPL_200_SERIES_IMPUTED_AND_ADJUSTED
-
-
-@pytest.fixture
-def score_col():
-    return field_names.SCORE_N_COMMUNITIES
-
-
-@pytest.fixture
-def score_col_with_donuts():
-    return field_names.FINAL_SCORE_N_BOOLEAN
-
-
-@pytest.fixture
-def donut_hole_score_only():
-    return field_names.SCORE_N_COMMUNITIES + field_names.ADJACENT_MEAN_SUFFIX
-
-
-@pytest.fixture
-def total_population_col():
-    return field_names.TOTAL_POP_FIELD
 
 
 def _helper_test_count_exceeding_threshold(df, col, error_check=1000):
@@ -143,94 +25,143 @@ def _helper_single_threshold_test(df, col, socioeconomic_column, score_column):
     return nulls_dont_exist, only_trues
 
 
-def _helper_test_thresholds(df, thresholds, ses_col, score_col_name):
-    """Tests to ensure that everything that "exceeds" burden threshold and socioeconomic threshold is properly labeled a DAC"""
-    for col in thresholds:
+@dataclass
+class ThreshholdTestConfig:
+    name: str
+    threshhold_columns: List[str]
+    ses_column_name: str = field_names.FPL_200_SERIES_IMPUTED_AND_ADJUSTED
+    score_column_name: str = field_names.SCORE_N_COMMUNITIES
+
+    @property
+    def error_message(self):
+        return f"Eligibility columns have an error, {self.name}"
+
+
+def check_for_threshhold_errors(
+    df: pd.DataFrame, config: ThreshholdTestConfig
+) -> List[str]:
+    errors = []
+    for col in config.threshhold_columns:
         nulls_dont_exist, only_trues = _helper_single_threshold_test(
-            df, col, ses_col, score_col_name
+            df,
+            col,
+            config.ses_column_name,
+            config.score_column_name,
         )
         proper_threshold_identification = (
             _helper_test_count_exceeding_threshold(df, col)
         )
-        assert (
-            nulls_dont_exist
-        ), f"For {col}, threshold is not calculated right -- there are NaNs in Score"
-        assert (
-            only_trues
-        ), f"For {col} and {ses_col}, threshold is not calculated right -- there are Falses where there should only be Trues"
-        assert proper_threshold_identification, (
-            f"Threshold {col} returns too few tracts, are you sure it's nationally-representative?",
+        if not nulls_dont_exist:
+            errors.append(
+                f"For {col}, threshold is not calculated right -- there are NaNs in Score"
+            )
+        if not only_trues:
+            errors.append(
+                f"For {col} and {config.ses_column_name}, threshold is not calculated right "
+                f"-- there are Falses where there should only be Trues"
+            )
+        if not proper_threshold_identification:
+            errors.append(
+                f"Threshold {col} returns too few tracts, are you sure it's nationally-representative?"
+            )
+    if errors:
+        errors.append(config.error_message)
+    return errors
+
+
+def test_threshholds(final_score_df):
+    climate_thresholds = ThreshholdTestConfig(
+        "climate",
+        [
+            field_names.EXPECTED_POPULATION_LOSS_EXCEEDS_PCTILE_THRESHOLD,
+            field_names.EXPECTED_AGRICULTURAL_LOSS_EXCEEDS_PCTILE_THRESHOLD,
+            field_names.EXPECTED_BUILDING_LOSS_EXCEEDS_PCTILE_THRESHOLD,
+            field_names.HIGH_FUTURE_FLOOD_RISK_FIELD,
+            field_names.HIGH_FUTURE_WILDFIRE_RISK_FIELD,
+        ],
+    )
+    energy_thresholds = ThreshholdTestConfig(
+        "energy",
+        [
+            field_names.ENERGY_BURDEN_EXCEEDS_PCTILE_THRESHOLD,
+            field_names.PM25_EXCEEDS_PCTILE_THRESHOLD,
+        ],
+    )
+    transportation_thresholds = ThreshholdTestConfig(
+        "transportation",
+        [
+            field_names.DIESEL_EXCEEDS_PCTILE_THRESHOLD,
+            field_names.DOT_BURDEN_PCTILE_THRESHOLD,
+            field_names.TRAFFIC_PROXIMITY_PCTILE_THRESHOLD,
+        ],
+    )
+    housing_thresholds = ThreshholdTestConfig(
+        "housing",
+        [
+            field_names.HISTORIC_REDLINING_SCORE_EXCEEDED,
+            field_names.NO_KITCHEN_OR_INDOOR_PLUMBING_PCTILE_THRESHOLD,
+            field_names.LEAD_PAINT_PROXY_PCTILE_THRESHOLD,
+            field_names.HOUSING_BURDEN_PCTILE_THRESHOLD,
+            field_names.NON_NATURAL_PCTILE_THRESHOLD,
+        ],
+    )
+    pollution_thresholds = ThreshholdTestConfig(
+        "pollution",
+        [
+            field_names.RMP_PCTILE_THRESHOLD,
+            field_names.NPL_PCTILE_THRESHOLD,
+            field_names.TSDF_PCTILE_THRESHOLD,
+            field_names.AML_BOOLEAN,
+            field_names.ELIGIBLE_FUDS_BINARY_FIELD_NAME,
+        ],
+    )
+    water_thresholds = ThreshholdTestConfig(
+        "water",
+        [
+            field_names.WASTEWATER_PCTILE_THRESHOLD,
+            field_names.UST_PCTILE_THRESHOLD,
+        ],
+    )
+    health_thresholds = ThreshholdTestConfig(
+        "health",
+        [
+            field_names.DIABETES_PCTILE_THRESHOLD,
+            field_names.ASTHMA_PCTILE_THRESHOLD,
+            field_names.HEART_DISEASE_PCTILE_THRESHOLD,
+            field_names.LOW_LIFE_EXPECTANCY_PCTILE_THRESHOLD,
+        ],
+    )
+    workforce_base_thresholds = ThreshholdTestConfig(
+        "workforce (not island areas)",
+        [
+            field_names.UNEMPLOYMENT_PCTILE_THRESHOLD,
+            field_names.LOW_MEDIAN_INCOME_PCTILE_THRESHOLD,
+            field_names.LINGUISTIC_ISOLATION_PCTILE_THRESHOLD,
+            field_names.POVERTY_PCTILE_THRESHOLD,
+        ],
+        ses_column_name=field_names.LOW_HS_EDUCATION_FIELD,
+    )
+    errors = []
+    for threshhold_config in [
+        climate_thresholds,
+        energy_thresholds,
+        transportation_thresholds,
+        housing_thresholds,
+        pollution_thresholds,
+        water_thresholds,
+        health_thresholds,
+        workforce_base_thresholds,
+    ]:
+        errors.extend(
+            check_for_threshhold_errors(final_score_df, threshhold_config)
         )
-        return True
+    error_text = "\n".join(errors)
+    assert not errors, error_text
 
 
-def test_climate_thresholds(
-    final_score_df, climate_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, climate_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, climate"
-
-
-def test_energy_thresholds(
-    final_score_df, energy_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, energy_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, energy"
-
-
-def test_transportation_thresholds(
-    final_score_df, transportation_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, transportation_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, transportation"
-
-
-def test_housing_thresholds(
-    final_score_df, housing_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, housing_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, housing"
-
-
-def test_pollution_thresholds(
-    final_score_df, pollution_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, pollution_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, pollution"
-
-
-def test_water_thresholds(
-    final_score_df, water_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, water_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, water"
-
-
-def test_health_thresholds(
-    final_score_df, health_thresholds, low_inc_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, health_thresholds, low_inc_ses, score_col
-    ), "Eligibility columns have an error, health"
-
-
-def test_workforce_base_thresholds(
-    final_score_df, workforce_base_thresholds, low_hs_ses, score_col
-):
-    assert _helper_test_thresholds(
-        final_score_df, workforce_base_thresholds, low_hs_ses, score_col
-    ), "Eligibility columns have an error, workforce (not island areas)"
-
-
-def test_max_40_percent_DAC(
-    final_score_df, score_col_with_donuts, total_population_col
-):
+def test_max_40_percent_DAC(final_score_df):
+    score_col_with_donuts = field_names.FINAL_SCORE_N_BOOLEAN
+    total_population_col = field_names.TOTAL_POP_FIELD
     assert (
         final_score_df[score_col_with_donuts].isna().sum() == 0
     ), f"Error: {score_col_with_donuts} contains NULLs"
@@ -245,9 +176,12 @@ def test_max_40_percent_DAC(
     ), "FYI: You've identified no tracts at all!"
 
 
-def test_donut_hole_addition_to_score_n(
-    final_score_df, donut_hole_score_only, score_col_with_donuts, score_col
-):
+def test_donut_hole_addition_to_score_n(final_score_df):
+    score_col_with_donuts = field_names.FINAL_SCORE_N_BOOLEAN
+    score_col = field_names.SCORE_N_COMMUNITIES
+    donut_hole_score_only = (
+        field_names.SCORE_N_COMMUNITIES + field_names.ADJACENT_MEAN_SUFFIX
+    )
     count_donuts = final_score_df[donut_hole_score_only].sum()
     count_n = final_score_df[score_col].sum()
     count_n_with_donuts = final_score_df[score_col_with_donuts].sum()
