a6514d198dbcd1d762699d9d928edcb47b8f4a84
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-08-26 13:21:15 -0400

    Add default timout to requests (#1846)

--- data/data-pipeline/data_pipeline/config.py
+++ data/data-pipeline/data_pipeline/config.py
@@ -12,7 +12,7 @@ settings = Dynaconf(
 
 # set root dir
 settings.APP_ROOT = pathlib.Path(data_pipeline.__file__).resolve().parent
-
+settings.REQUESTS_DEFAULT_TIMOUT = 3600
 # To set an environment use:
 # Linux/OSX: export ENV_FOR_DYNACONF=staging
 # Windows: set ENV_FOR_DYNACONF=staging
