561e9d5efadf26913af6c338d78892f62e085fa2
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-08-29 16:20:06 -0400

    No need for low memeory (#1848)

--- data/data-pipeline/data_pipeline/tests/score/fixtures.py
+++ data/data-pipeline/data_pipeline/tests/score/fixtures.py
@@ -9,4 +9,5 @@ def final_score_df():
     return pd.read_csv(
         settings.APP_ROOT / "data" / "score" / "csv" / "full" / "usa.csv",
         dtype={field_names.GEOID_TRACT_FIELD: str},
+        low_memory=False,
     )
