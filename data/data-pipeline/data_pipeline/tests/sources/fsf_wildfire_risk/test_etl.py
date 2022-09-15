2ad7a7d5e5564458e33f4ed3d7b9519155518342
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-09-15 12:21:16 -0400

    Test clipping and zfill behavior (#1848)

--- data/data-pipeline/data_pipeline/tests/sources/fsf_wildfire_risk/test_etl.py
+++ data/data-pipeline/data_pipeline/tests/sources/fsf_wildfire_risk/test_etl.py
@@ -10,6 +10,9 @@ class TestWildfireRiskETL(TestETL):
     _SAMPLE_DATA_FILE_NAME = "fsf_fire/fire-tract2010.csv"
     _SAMPLE_DATA_ZIP_FILE_NAME = "fsf_fire.zip"
     _EXTRACT_TMP_FOLDER_NAME = "WildfireRiskETL"
+    _FIXTURES_SHARED_TRACT_IDS = TestETL._FIXTURES_SHARED_TRACT_IDS + [
+        "04003001402"  # A tract with 1 property, also missing a digit
+    ]
 
     def setup_method(self, _method, filename=__file__):
         """Invoke `setup_method` from Parent, but using the current file name.
