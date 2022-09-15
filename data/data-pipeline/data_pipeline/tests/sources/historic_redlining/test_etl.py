c74f68df001f2516309b576c0c1fdd2ae25f5866
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-09-15 13:31:07 -0400

    Fix bad test assumption (#1848)

--- data/data-pipeline/data_pipeline/tests/sources/historic_redlining/test_etl.py
+++ data/data-pipeline/data_pipeline/tests/sources/historic_redlining/test_etl.py
@@ -1,3 +1,4 @@
+# pylint: disable=protected-access
 import pathlib
 import pandas as pd
 from data_pipeline.tests.sources.example.test_etl import TestETL
@@ -31,3 +32,35 @@ class TestHistoricRedliningETL(TestETL):
             dtype={etl.GEOID_TRACT_FIELD_NAME: str},
         )
         assert tmp_df.shape == (15, 5)
+
+    def test_load_base(self, snapshot, mock_etl, mock_paths):
+        """Test load method.
+        We need to run transform here for real to add 
+        the dynamic cols to keep
+        """
+        # setup - input variables
+        etl = self._setup_etl_instance_and_run_extract(
+            mock_etl=mock_etl,
+            mock_paths=mock_paths,
+        )
+        etl.transform()
+        etl.load()
+
+        # Make sure it creates the file.
+        actual_output_path = etl._get_output_file_path()
+        assert actual_output_path.exists()
+
+        # Check COLUMNS_TO_KEEP remain
+        actual_output = pd.read_csv(
+            actual_output_path, dtype={etl.GEOID_TRACT_FIELD_NAME: str}
+        )
+
+        for col in etl.COLUMNS_TO_KEEP:
+            assert col in actual_output.columns, f"{col} is missing from output"
+
+        # Check the snapshots
+        snapshot.snapshot_dir = self._DATA_DIRECTORY_FOR_TEST
+        snapshot.assert_match(
+            actual_output.to_csv(index=False, float_format=self._FLOAT_FORMAT),
+            self._OUTPUT_CSV_FILE_NAME,
+        )
