09f7ee28b969da0acb7a3ff9dcc7c6a5452f94fd
Vim USDS <vimal.k.shah@omb.eop.gov>
2022-09-27 15:52:54 -0700

    Update navigation links
    - adds hover and current effects
    - fits width of each nav link to content

--- client/src/components/J40Header/J40Header.tsx
+++ client/src/components/J40Header/J40Header.tsx
@@ -12,13 +12,13 @@ import BetaBanner from '../BetaBanner';
 import J40MainGridContainer from '../J40MainGridContainer';
 import GovernmentBanner from '../GovernmentBanner';
 import Language from '../Language';
-import {useWindowSize} from 'react-use';
+// import {useWindowSize} from 'react-use';
 
 // @ts-ignore
 import siteLogo from '../../images/j40-logo-v2.png';
 import * as styles from './J40Header.module.scss';
 import * as COMMON_COPY from '../../data/copy/common';
-import {PAGES_ENDPOINTS, USWDS_BREAKPOINTS} from '../../data/constants';
+import {PAGES_ENDPOINTS} from '../../data/constants';
 
 /**
  * The J40Header component will control how the header looks for both mobile and desktop
@@ -32,8 +32,9 @@ import {PAGES_ENDPOINTS, USWDS_BREAKPOINTS} from '../../data/constants';
  */
 const J40Header = () => {
   const intl = useIntl();
-  const {width} = useWindowSize();
-
+  // const {width} = useWindowSize();
+  console.log('location:', location);
+  console.log('PAGES_ENDPOINTS.METHODOLOGY:', PAGES_ENDPOINTS.METHODOLOGY);
   // Logo text
   const logoLine1 = intl.formatMessage(COMMON_COPY.HEADER.TITLE_LINE_1);
 
@@ -41,8 +42,10 @@ const J40Header = () => {
    * State variable to control the toggling of mobile menu button
    */
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
-  const toggleMobileNav = (): void =>
+  const toggleMobileNav = (): void => {
+    console.log('mobile opened!');
     setMobileNavOpen((prevOpen) => !prevOpen);
+  };
 
   /**
    * State variable to hold the open/close state of each nav dropdown. This will allow for two
@@ -51,44 +54,30 @@ const J40Header = () => {
    * index 0 = Data & Methodology dropdown
    * index 1 = About dropdown
    */
-  const [isOpen, setIsOpen] = useState([true]);
+  const [isOpen, setIsOpen] = useState([false, false]);
+
+  /**
+   * This toggle function will handle both navigation toggle links (Meth and About).
+   *
+   * @param {number} index
+   */
   const onToggle = (index: number): void => {
+    console.log('🚀 ~ file: J40Header.tsx ~ line 57 ~ J40Header ~ isOpen before toggle', isOpen);
+
+    // The setIsOpen is used to toggle the currently selected nav link
     setIsOpen((prevIsOpen) => {
-      const newIsOpen = [true];
+      const newIsOpen = [...isOpen];
       newIsOpen[index] = !prevIsOpen[index];
       return newIsOpen;
     });
+
+    // This logic handles closing an already open nav link
+    if (index === 0 && isOpen[1] === true) {
+      setIsOpen([isOpen[0], false]);
+    } else if (index === 1 && isOpen[0] === true) {
+      setIsOpen([false, isOpen[1]]);
+    }
   };
-
-  /**
-   * This is the array that holds the navigation links and eventually is the one
-   * that is passed to the render function. It only defines Explore and
-   * Contact.
-   *
-   * The Methodology & Data link along with the About link is passed in depending on
-   * screen size. These will be spliced into this navLinks array manually.
-   *
-   */
-  const navLinks = [
-    <Link
-      to={PAGES_ENDPOINTS.EXPLORE}
-      key={'explore-map'}
-      activeClassName="usa-current"
-      data-cy={'nav-link-explore-the-map'}>
-      {intl.formatMessage(COMMON_COPY.HEADER.EXPLORE)}
-    </Link>,
-    <Link
-      to={PAGES_ENDPOINTS.CONTACT}
-      key={'contact'}
-      activeClassName="usa-current"
-      data-cy={'nav-link-contact'}>
-      {intl.formatMessage(COMMON_COPY.HEADER.CONTACT)}
-    </Link>,
-    <div key={'language'}>
-      <Language isDesktop={false}/>
-    </div>,
-  ];
-
 
   /**
    * On mobile, the Methodology & Data page should have two sub-nav links. This defines
@@ -147,14 +136,31 @@ const J40Header = () => {
   ];
 
 
-  /**
-   * Create the mobile/desktop components for Meth&Data and About respectively
+  /*
+   * This is the array that holds the navigation links and eventually is the one
+   * that is passed to the render function. It only defines Explore and
+   * Contact.
+   *
+   * The Methodology & Data link along with the About link is passed in depending on
+   * screen size. These will be spliced into this navLinks array manually.
+   *
    */
-
-  // Methodology & Data component on mobile (with sub-pages)
-  const MethNavMobile = () =>
+  const navLinks = [
+    <Link
+      to={PAGES_ENDPOINTS.EXPLORE}
+      key={'explore-map'}
+      activeClassName="usa-current"
+      data-cy={'nav-link-explore-the-map'}>
+      {intl.formatMessage(COMMON_COPY.HEADER.EXPLORE)}
+    </Link>,
     <>
       <NavDropDownButton
+        className={
+          location.pathname === `/en${PAGES_ENDPOINTS.METHODOLOGY}` ||
+          location.pathname === `/en${PAGES_ENDPOINTS.DOWNLOADS}` ?
+          'usa-current' :
+          ''
+        }
         key="methDropDown"
         label={intl.formatMessage(COMMON_COPY.HEADER.METHODOLOGY)}
         menuId="methMenu"
@@ -170,54 +176,116 @@ const J40Header = () => {
         isOpen={isOpen[0]}
       >
       </Menu>
-    </>;
-
-  // About component on mobile (with sub-pages)
-  const AboutNavMobile = () =>
+    </>,
     <>
       <NavDropDownButton
-        key="methDropDown"
+        className={
+          location.pathname === `/en${PAGES_ENDPOINTS.ABOUT}` ||
+          location.pathname === `/en${PAGES_ENDPOINTS.FAQS}` ||
+          location.pathname === `/en${PAGES_ENDPOINTS.PUBLIC_ENG}` ?
+          'usa-current' :
+          ''
+        }
+        key="aboutDropDown"
         label={intl.formatMessage(COMMON_COPY.HEADER.ABOUT)}
-        menuId="methMenu"
+        menuId="aboutMenu"
         isOpen={isOpen[1]}
         onToggle={(): void => onToggle(1)}
-        data-cy={'nav-dropdown-methodology'}
+        data-cy={'nav-dropdown-about'}
       >
       </NavDropDownButton>
       <Menu
-        id='methMenu'
+        id='aboutMenu'
         type='subnav'
         items={aboutPageSubNavLinks}
         isOpen={isOpen[1]}
       >
       </Menu>
-    </>;
+    </>,
+    <Link
+      to={PAGES_ENDPOINTS.CONTACT}
+      key={'contact'}
+      activeClassName="usa-current"
+      data-cy={'nav-link-contact'}>
+      {intl.formatMessage(COMMON_COPY.HEADER.CONTACT)}
+    </Link>,
+    <div key={'language'}>
+      <Language isDesktop={false}/>
+    </div>,
+  ];
+
+
+  /**
+   * Create the mobile/desktop components for Meth&Data and About respectively
+   */
+
+  // Methodology & Data component on mobile (with sub-pages)
+  // const MethNavMobile = () =>
+  //   <>
+  //     <NavDropDownButton
+  //       key="methDropDown"
+  //       label={intl.formatMessage(COMMON_COPY.HEADER.METHODOLOGY)}
+  //       menuId="methMenu"
+  //       isOpen={isOpen[0]}
+  //       onToggle={(): void => onToggle(0)}
+  //       data-cy={'nav-dropdown-methodology'}
+  //     >
+  //     </NavDropDownButton>
+  //     <Menu
+  //       id='methMenu'
+  //       type='subnav'
+  //       items={methPageSubNavLinks}
+  //       isOpen={isOpen[0]}
+  //     >
+  //     </Menu>
+  //   </>;
+
+  // About component on mobile (with sub-pages)
+  // const AboutNavMobile = () =>
+  //   <>
+  //     <NavDropDownButton
+  //       key="methDropDown"
+  //       label={intl.formatMessage(COMMON_COPY.HEADER.ABOUT)}
+  //       menuId="methMenu"
+  //       isOpen={isOpen[1]}
+  //       onToggle={(): void => onToggle(1)}
+  //       data-cy={'nav-dropdown-methodology'}
+  //     >
+  //     </NavDropDownButton>
+  //     <Menu
+  //       id='methMenu'
+  //       type='subnav'
+  //       items={aboutPageSubNavLinks}
+  //       isOpen={isOpen[1]}
+  //     >
+  //     </Menu>
+  //   </>;
 
   // Methodology & Data component on desktop (no sub-pages)
-  const MethNavDesktop = () =>
-    <Link
-      to={PAGES_ENDPOINTS.METHODOLOGY}
-      key={'methodology'}
-      activeClassName="usa-current"
-      data-cy={'nav-link-methodology'}>
-      {intl.formatMessage(COMMON_COPY.HEADER.METHODOLOGY)}
-    </Link>;
+  // const MethNavDesktop = () =>
+  //   <Link
+  //     to={PAGES_ENDPOINTS.METHODOLOGY}
+  //     key={'methodology'}
+  //     activeClassName="usa-current"
+  //     data-cy={'nav-link-methodology'}>
+  //     {intl.formatMessage(COMMON_COPY.HEADER.METHODOLOGY)}
+  //   </Link>;
 
-  // About component on desktop (no sub-pages)
-  const AboutNavDesktop = () =>
-    <Link
-      to={PAGES_ENDPOINTS.ABOUT}
-      key={'about'}
-      activeClassName="usa-current"
-      data-cy={'nav-link-about'}>
-      {intl.formatMessage(COMMON_COPY.HEADER.ABOUT)}
-    </Link>;
+  // // About component on desktop (no sub-pages)
+  // const AboutNavDesktop = () =>
+  //   <Link
+  //     to={PAGES_ENDPOINTS.ABOUT}
+  //     key={'about'}
+  //     activeClassName="usa-current"
+  //     data-cy={'nav-link-about'}>
+  //     {intl.formatMessage(COMMON_COPY.HEADER.ABOUT)}
+  //   </Link>;
 
   // Modify navLinks to choose the appropriate Methodology & Data component, depending on screen size
-  navLinks.splice(1, 0, width > USWDS_BREAKPOINTS.DESKTOP ? <MethNavDesktop/> : <MethNavMobile/>);
+  // navLinks.splice(1, 0, width > USWDS_BREAKPOINTS.DESKTOP ? <MethNavMobile/> : <MethNavMobile/>);
 
   // Modify navLinks to choose the appropriate About component, depending on screen size
-  navLinks.splice(2, 0, width > USWDS_BREAKPOINTS.DESKTOP ? <AboutNavDesktop/> : <AboutNavMobile/>);
+  // navLinks.splice(2, 0, width > USWDS_BREAKPOINTS.DESKTOP ? <AboutNavMobile/> : <AboutNavMobile/>);
 
   return (
     <Header basic={true} role={'banner'}>
