2d11b6dda2b6edae6b8a8ad205dfc2f55456a5b8
Jorge Escobar <jorge.e.escobar@omb.eop.gov>
2022-10-21 12:31:17 -0400

    debugging

--- data/data-pipeline/data_pipeline/etl/score/etl_score_geo.py
+++ data/data-pipeline/data_pipeline/etl/score/etl_score_geo.py
@@ -370,6 +370,11 @@ class GeoScoreETL(ExtractTransformLoad):
 
             # Per #1557
             # zip file that contains the shapefiles, codebook and checksum file
+            logger.info("Dump of download fokder files")
+            for file in os.listdir(constants.SCORE_DOWNLOADABLE_DIR):
+                logger.info(str(file))
+            logger.info(f"codebook_path: {codebook_path}")
+
             codebook_path = constants.SCORE_DOWNLOADABLE_CODEBOOK_FILE_PATH
             readme_path = constants.SCORE_VERSIONING_README_FILE_PATH
             version_shapefile_codebook_zip_path = (
