d22496889f3a2be3d4f2052a84f146b7d580c98c
matt bowen <matthew.r.bowen@omb.eop.gov>
2022-10-26 15:50:40 -0400

    Floor in a simpler way (#2033)
    Emma pointed out that all teh stuff we're doing in floor_series is
    probably unnecessary for this case, so just use the built-in floor.

--- data/data-pipeline/data_pipeline/etl/score/etl_score_post.py
+++ data/data-pipeline/data_pipeline/etl/score/etl_score_post.py
@@ -236,13 +236,10 @@ class PostScoreETL(ExtractTransformLoad):
             for col, col_dtype in score_tiles.dtypes.items()
             if col_dtype == np.dtype("float64")
         ]
-        score_tiles[float_cols] = score_tiles[float_cols].apply(
-            func=lambda series: floor_series(
-                series=series,
-                number_of_decimals=constants.TILES_ROUND_NUM_DECIMALS,
-            ),
-            axis=0,
-        )
+        scale_factor = 10**constants.TILES_ROUND_NUM_DECIMALS
+        score_tiles[float_cols] = (
+            score_tiles[float_cols] * scale_factor
+        ).apply(np.floor) / scale_factor
 
         logger.info("Adding fields for island areas and Puerto Rico")
         # The below operation constructs variables for the front end.
